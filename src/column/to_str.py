4a30d7073c5f3b119021e0696b4fc2351c7a6d4c
John Forbes <john.robert.forbes@gmail.com>
2023-08-11 15:21:52 +1000

    Updated src/column/to_str.py.

--- src/column/to_str.py
+++ src/column/to_str.py
@@ -2,6 +2,7 @@ from hak.pxyz import f as pxyz
 from src.column.make import f as make_column
 from src.column.width.get import f as get_width
 from hak.pf import f as pf
+from datetime import date
 
 def f(x):
   w = get_width(x) + 2
@@ -24,7 +25,47 @@ def f(x):
     '-'*w,
   ])
 
-def t_a():
+def t_0():
+  x = make_column('a', [0])
+  y = '\n'.join([
+    '---',
+    ' a ',
+    '---',
+    ' 0 ',
+    '---',
+  ])
+  z = f(x)
+  return pxyz(x, '\n'+y, '\n'+z)
+
+def t_1():
+  x = make_column('abc', [0, 10, 23])
+  y = '\n'.join([
+    '-----',
+    ' abc ',
+    '-----',
+    '   0 ',
+    '  10 ',
+    '  23 ',
+    '-----',
+  ])
+  z = f(x)
+  return pxyz(x, '\n'+y, '\n'+z)
+
+def t_2():
+  x = make_column('ab', [0, 100, 2300])
+  y = '\n'.join([
+    '------',
+    '   ab ',
+    '------',
+    '    0 ',
+    '  100 ',
+    ' 2300 ',
+    '------',
+  ])
+  z = f(x)
+  return pxyz(x, '\n'+y, '\n'+z)
+
+def t_3():
   x = make_column('apples', [_**_ for _ in range(10)])
   y = '\n'.join([
     '-----------',
@@ -45,7 +86,7 @@ def t_a():
   z = f(x)
   return pxyz(x, y, z, new_line=True)
 
-def t_b():
+def t_4():
   x = make_column(
     'animal',
     [
@@ -66,7 +107,28 @@ def t_b():
   z = f(x)
   return pxyz(x, y, z, new_line=True)
 
+def t_date():
+  x = make_column(
+    'date',
+    [date(2023, 1, 1), date(2023, 4, 8), date(2023, 12, 31)]
+  )
+  y = '\n'.join([
+    '------------',
+    '       date ',
+    '------------',
+    ' 2023-01-01 ',
+    ' 2023-04-08 ',
+    ' 2023-12-31 ',
+    '------------',
+  ])
+  z = f(x)
+  return pxyz(x, '\n'+y, '\n'+z)
+
 def t():
-  if not t_a(): return pf('!t_a')
-  if not t_b(): return pf('!t_b')
+  if not t_0(): return pf('!t_0')
+  if not t_1(): return pf('!t_1')
+  if not t_2(): return pf('!t_2')
+  if not t_3(): return pf('!t_3')
+  if not t_4(): return pf('!t_4')
+  if not t_date(): return pf('!t_date')
   return True
