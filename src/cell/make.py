8f7cea1e4172d7e832614411060d26ac7ac47944
John Forbes <john.robert.forbes@gmail.com>
2023-08-11 17:20:58 +1000

    Updated src/cell/make.py.

--- src/cell/make.py
+++ src/cell/make.py
@@ -1,18 +1,26 @@
+from hak.one.get_datatype import f as detect_type
 from hak.pxyz import f as pxyz
 from hak.pf import f as pf
 from hak.one.dict.rate.make import f as make_rate
 
-f = lambda value: {'value': value}
+def f(value, datatype=None):
+  y = {}
+  y['value'] = value
+  y['datatype'] = datatype or detect_type(value)
+  return y
 
 def t_0():
   x = 0
-  y = {'value': 0}
+  y = {'value': 0, 'datatype': 'int'}
   z = f(x)
   return pxyz(x, y, z)
 
 def t_rate():
   x = make_rate(numerator=1, denominator=1, unit={'m': 1})
-  y = {'value': {'numerator': 1, 'denominator': 1, 'unit': {'m': 1}}}
+  y = {
+    'value': {'numerator': 1, 'denominator': 1, 'unit': {'m': 1}},
+    'datatype': 'rate'
+  }
   z = f(x)
   return pxyz(x, y, z)
 
