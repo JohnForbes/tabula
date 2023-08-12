3b05f3d73f77ea62924a6025244e726c09bc8c9a
John Forbes <john.robert.forbes@gmail.com>
2023-08-12 10:08:28 +1000

    Updated src/table/insert_record.py.

--- src/table/insert_record.py
+++ src/table/insert_record.py
@@ -4,8 +4,8 @@ from hak.one.list.append_if_not_present import f as append_if_not_present
 from src.cell.make import f as make_cell
 
 def f(table, record):
-  last_row_identifier = table['rows'][-1] if table['rows'] else -1
-  row_identifier = last_row_identifier+1
+  max_row_identifier = max(table['rows']) if table['rows'] else -1
+  row_identifier = max_row_identifier+1
   table['rows'] = table['rows'] + [row_identifier]
 
   for (k, v) in record.items():
