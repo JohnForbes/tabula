479c2ce1c354ebcc8d40f870cb2ff1d16bd602fc
John Forbes <john.robert.forbes@gmail.com>
2023-08-12 12:59:16 +1000

    Updated src/table/make.py.

--- src/table/make.py
+++ src/table/make.py
@@ -3,19 +3,15 @@ from hak.one.dict.is_a import f as is_dict
 from hak.one.list.is_a import f as is_list
 
 # __init__
-f = lambda: {
-  'column_names': [],
-  'row_names': [],
-  'cells': {}
-}
+f = lambda: {'column_order': [], 'row_order': [], 'cells': {}}
 
 def t():
   z = f()
   if not is_dict(z): return pf('not is_dict(z)')
-  if 'column_names' not in z: return pf("'column_names' not in z")
-  if not is_list(z['column_names']): return pf("not is_list(z['column_names'])")
-  if 'row_names' not in z: return pf("'row_names' not in z")
-  if not is_list(z['row_names']): return pf("not is_list(z['row_names'])")
+  if 'column_order' not in z: return pf("'column_order' not in z")
+  if not is_list(z['column_order']): return pf("not is_list(z['column_order'])")
+  if 'row_order' not in z: return pf("'row_order' not in z")
+  if not is_list(z['row_order']): return pf("not is_list(z['row_order'])")
   if 'cells' not in z: return pf("'cells' not in z")
   if not is_dict(z['cells']): return pf("not is_dict(z['cells'])")
   return True
