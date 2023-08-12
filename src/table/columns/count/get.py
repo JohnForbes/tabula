853c249fa7504a944387856899f7038c67eb8468
John Forbes <john.robert.forbes@gmail.com>
2023-08-12 12:28:01 +1000

    Updated src/table/columns/count/get.py.

--- src/table/columns/count/get.py
+++ src/table/columns/count/get.py
@@ -1,9 +1,10 @@
-from src.table.make import f as make_table
 from hak.pxyz import f as pxyz
+
 from src.table.insert_record import f as insert_record
+from src.table.make import f as make_table
 
 # column_count
-f = lambda x: len(x['column_names'])
+f = lambda x: len(x['column_order'])
 
 def t():
   x = insert_record(make_table(), {'a': 0, 'b': 1, 'c': 2})
