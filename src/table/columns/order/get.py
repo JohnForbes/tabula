549fbeaa06b94c6c304e2ca8b76bcccf43632b88
John Forbes <john.robert.forbes@gmail.com>
2023-08-12 12:29:13 +1000

    Updated src/table/columns/order/get.py.

--- src/table/columns/order/get.py
+++ src/table/columns/order/get.py
@@ -3,7 +3,7 @@ from hak.pxyz import f as pxyz
 from src.table.insert_record import f as insert_record
 
 # column_order
-f = lambda x: x['column_names']
+f = lambda x: x['column_order']
 
 def t():
   x = insert_record(make_table(), {'a': 0, 'b': 1, 'c': 2})
