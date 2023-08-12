8b24f4679c26bf94b73d198e74a064e7913f3027
John Forbes <john.robert.forbes@gmail.com>
2023-08-12 13:00:08 +1000

    Updated src/table/rows/count/get.py.

--- src/table/rows/count/get.py
+++ src/table/rows/count/get.py
@@ -1,11 +1,18 @@
-from src.table.make import f as make_table
 from hak.pxyz import f as pxyz
 
+from src.table.insert_records import f as insert_records
+from src.table.make import f as make_table
+
 # row_count
-f = lambda x: len(x['rows'])
+f = lambda x: len(x['row_order'])
 
 def t():
-  x = make_table(columns=['a', 'b', 'c'], rows=[0, 1, 2, 3])
+  x = insert_records(make_table(), [
+    {'a': 0, 'b':  1, 'c':  2},
+    {'a': 3, 'b':  4, 'c':  5},
+    {'a': 6, 'b':  7, 'c':  8},
+    {'a': 9, 'b': 10, 'c': 11}
+  ])
   y = 4
   z = f(x)
   return pxyz(x, y, z)
