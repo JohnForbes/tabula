b8181580835cdfa36aaf7fe80b37c79916d302df
John Forbes <john.robert.forbes@gmail.com>
2023-08-13 12:06:41 +1000

    Updated src/row/to_str.py.

--- src/row/to_str.py
+++ src/row/to_str.py
@@ -2,15 +2,10 @@ from hak.pxyz import f as pxyz
 from src.cell.to_str import f as cell_to_str
 from src.cell.make import f as make_cell
 
-def f(x):
-  return ' | '.join([cell_to_str(x[k]) for k in x])
+f = lambda x: '| '+' | '.join([cell_to_str(x[k]) for k in x])+' |'
 
 def t():
-  x = {
-    'a': make_cell(1),
-    'b': make_cell(2),
-    'c': make_cell(3),
-  }
-  y = '1 | 2 | 3'
+  x = {'a': make_cell(1), 'b': make_cell(2), 'c': make_cell(3)}
+  y = '| 1 | 2 | 3 |'
   z = f(x)
   return pxyz(x, y, z)
