c97512b171e9b8c24a2c257c2b3eec930cb08c3c
John Forbes <john.robert.forbes@gmail.com>
2023-08-13 12:14:39 +1000

    Updated src/table/to_str.py.

--- src/table/to_str.py
+++ src/table/to_str.py
@@ -1,31 +1,26 @@
 from hak.pf import f as pf
 from hak.pxyz import f as pxyz
-from src.row.to_str import f as row_to_str
 from src.table.insert_records import f as insert_records
 from src.table.make import f as make_table
-from src.table.row_name_to_row import f as row_name_to_row
 from src.table.to_hbar import f as table_to_hbar
 from src.table.to_header_str import f as table_to_header_str
+from src.table.to_rows_str import f as table_to_rows_str
 
 # __str__
-def f(table):
-  rows = [row_name_to_row(table, row_name) for row_name in table['row_order']]
-  return '\n'.join([
-    table_to_hbar(table),
-    table_to_header_str(table),
-    table_to_hbar(table),
-    *['| '+row_to_str(r)+' |' for r in rows],
-    table_to_hbar(table),
-  ])
+f = lambda x: '\n'.join([
+  table_to_hbar(x),
+  table_to_header_str(x),
+  table_to_hbar(x),
+  table_to_rows_str(x),
+  table_to_hbar(x),
+])
 
 def t_ab():
-  x = {
-    'table': insert_records(make_table(), [
-      {'a': 0, 'b': 1},
-      {'a': 3, 'b': 4},
-      {'a': 6, 'b': 7}
-    ])
-  }
+  x = insert_records(make_table(), [
+    {'a': 0, 'b': 1},
+    {'a': 3, 'b': 4},
+    {'a': 6, 'b': 7}
+  ])
   y = '\n'.join([
     '|---|---|',
     '| a | b |',
@@ -35,17 +30,15 @@ def t_ab():
     '| 6 | 7 |',
     '|---|---|',
   ])
-  z = f(**x)
+  z = f(x)
   return pxyz(x, y, z, new_line=1)
 
 def t_ac():
-  x = {
-    'table': insert_records(make_table(), [
-      {'a': 0, 'c': 2},
-      {'a': 3, 'c': 5},
-      {'a': 6, 'c': 8}
-    ])
-  }
+  x = insert_records(make_table(), [
+    {'a': 0, 'c': 2},
+    {'a': 3, 'c': 5},
+    {'a': 6, 'c': 8}
+  ])
   y = '\n'.join([
     '|---|---|',
     '| a | c |',
@@ -55,17 +48,15 @@ def t_ac():
     '| 6 | 8 |',
     '|---|---|',
   ])
-  z = f(**x)
+  z = f(x)
   return pxyz(x, y, z, new_line=1)
 
 def t_abc():
-  x = {
-    'table': insert_records(make_table(), [
-      {'a': 0, 'b': 1, 'c': 2},
-      {'a': 3, 'b': 4, 'c': 5},
-      {'a': 6, 'b': 7, 'c': 8}
-    ])
-  }
+  x = insert_records(make_table(), [
+    {'a': 0, 'b': 1, 'c': 2},
+    {'a': 3, 'b': 4, 'c': 5},
+    {'a': 6, 'b': 7, 'c': 8}
+  ])
   y = '\n'.join([
     '|---|---|---|',
     '| a | b | c |',
@@ -75,7 +66,7 @@ def t_abc():
     '| 6 | 7 | 8 |',
     '|---|---|---|',
   ])
-  z = f(**x)
+  z = f(x)
   return pxyz(x, y, z, new_line=1)
 
 def t():
