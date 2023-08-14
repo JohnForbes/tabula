d312a642d595cf9b3bbb25e4f3cba9b67480108c
John Forbes <john.robert.forbes@gmail.com>
2023-08-14 12:02:38 +1000

    Updated src/column/width/get.py.

--- src/column/width/get.py
+++ src/column/width/get.py
@@ -1,6 +1,6 @@
+from hak.pf import f as pf
 from hak.pxyz import f as pxyz
 from src.column.make import f as make_column
-from hak.pf import f as pf
 
 # width
 f = lambda x: max(len(x['name']), *[len(str(v)) for v in x['values']])
@@ -17,7 +17,7 @@ def t_values_dominant():
   z = f(x)
   return pxyz(x, y, z)
 
-def t_width_heading_dominant():
+def t_name_dominant():
   x = make_column('abc', [0, 1, 2, 3])
   y = 3
   z = f(x)
@@ -25,6 +25,6 @@ def t_width_heading_dominant():
 
 def t():
   if not t_a(): return pf('!t_a')
+  if not t_name_dominant(): return pf('!t_name_dominant')
   if not t_values_dominant(): return pf('!t_values_dominant')
-  if not t_width_heading_dominant(): return pf('!t_width_heading_dominant')
   return True
