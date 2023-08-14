7f50ae0db1112d358a50cfc3520f54e7891b3033
John Forbes <john.robert.forbes@gmail.com>
2023-08-14 12:14:22 +1000

    Updated src/column/make.py.

--- src/column/make.py
+++ src/column/make.py
@@ -1,22 +1,18 @@
 from hak.many.values.detect_type import f as detect_type
 from hak.one.string.is_a import f as is_str
-from hak.one.tuple.is_a import f as is_tup
-from src.column.is_a import f as is_column
+from hak.one.tuple.is_a import f as is_tuple
 from hak.pf import f as pf
 from hak.pxyz import f as pxyz
+from src.column.is_a import f as is_column
 
-def f(name, values, datatype=None, path=None):
-  y = {
-    'name': name,
-    'values': values,
-    'datatype': datatype if datatype else detect_type(values)
-  }
+def f(name, values, path=None):
+  y = {'name': name, 'values': values, 'datatype': detect_type(values)}
 
   if path:
-    if not any([is_tup(path), is_str(path)]):
+    if not any([is_tuple(path), is_str(path)]):
       raise NotImplementedError('Unexpected path type')
 
-  y['path'] = (path if is_tup(path) else tuple([path])) if path else tuple()
+  y['path'] = (path if is_tuple(path) else tuple([path])) if path else tuple()
   return y
 
 def t_a():
@@ -53,9 +49,40 @@ def t_d():
   z = f(**x)
   return pxyz(x, y, z)
 
+def t_0():
+  x = {'name': 'abc', 'values': [0, 1, 2]}
+  y = {'name': 'abc', 'values': [0, 1, 2], 'datatype': 'int', 'path': ()}
+  z = f(**x)
+  return pxyz(x, y, z)
+
+def t_path_as_str():
+  x = {'name': 'abc', 'values': [0, 1, 2], 'path': 'root'}
+  y = {
+    'name': 'abc',
+    'values': [0, 1, 2],
+    'datatype': 'int',
+    'path': ('root',)
+  }
+  z = f(**x)
+  return pxyz(x, y, z)
+
+def t_path():
+  x = {'name': 'abc', 'values': [0, 1, 2], 'path': ('root', 'branch')}
+  y = {
+    'name': 'abc',
+    'values': [0, 1, 2],
+    'datatype': 'int',
+    'path': ('root', 'branch')
+  }
+  z = f(**x)
+  return pxyz(x, y, z)
+
 def t():
   if not t_a(): return pf('!t_a')
   if not t_b(): return pf('!t_b')
   if not t_c(): return pf('!t_c')
   if not t_d(): return pf('!t_d')
+  if not t_0(): return pf('!t_0')
+  if not t_path(): return pf('!t_path')
+  if not t_path_as_str(): return pf('!t_path_as_str')
   return True
