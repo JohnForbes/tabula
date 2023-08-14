ba3b7df6a4a6eaa5ab87febd3adab9e54b88270e
John Forbes <john.robert.forbes@gmail.com>
2023-08-14 12:58:27 +1000

    Updated src/cell/to_str.py.

--- src/cell/to_str.py
+++ src/cell/to_str.py
@@ -1,17 +1,25 @@
-from src.cell.make import f as make_cell
-from hak.pxyz import f as pxyz
-from hak.pf import f as pf
-# from hak.one.bool.is_a import f as is_bool
 # from hak.one.dict.get_or_zero import f as v_or_0
-# from hak.one.dict.rate.is_a import f as is_a_rate
-# from hak.one.dict.rate.to_str import f as rate_to_str
-# from hak.one.number.float.is_a import f as is_float
-# from hak.one.string.colour.bright.green import f as g
-# from hak.one.string.colour.bright.red import f as r
 # from hak.one.number.int.is_a import f as is_int
+from hak.one.bool.is_a import f as is_bool
+from hak.one.dict.rate.is_a import f as is_a_rate
+from hak.one.dict.rate.make import f as make_rate
+from hak.one.dict.rate.to_str import f as rate_to_str
+from hak.one.number.float.is_a import f as is_float
+from hak.one.string.colour.bright.green import f as g
+from hak.one.string.colour.bright.red import f as r
+from hak.pf import f as pf
+from hak.pxyz import f as pxyz
+from src.cell.make import f as make_cell
 
+# src.cell.to_str
 # to_str
-f = lambda x: str(x['value'])
+def f(x):
+  if x['value'] is None: return ' '
+  if is_a_rate(x['value']): return rate_to_str(x['value'])
+  if is_bool(x['value']): return g('Y') if x['value'] else r('N')
+  if x['value'] == 0: return ' '
+  if is_float(x['value']): return f"{x['value']:.2f}"
+  return str(x['value'])
 
 # def f(x):
 #   if is_int(x['value']): return str(x['value'])
@@ -33,7 +41,7 @@ def t_a():
 
 def t_0():
   x = {'value': 0}
-  y = '0'
+  y = ' '
   z = f(make_cell(**x))
   return pxyz(x, y, z)
 
@@ -43,8 +51,50 @@ def t_10():
   z = f(make_cell(**x))
   return pxyz(x, y, z)
 
+def t_01():
+  x = {'value': 'a'}
+  y = str(x['value'])
+  z = f(make_cell(**x))
+  return pxyz(x, y, z)
+
+def t_02():
+  x = {'value': False}
+  y = r('N')
+  z = f(make_cell(**x))
+  return pxyz(x, y, z)
+
+def t_03():
+  x = {'value': True}
+  y = g('Y')
+  z = f(make_cell(**x))
+  return pxyz(x, y, z)
+
+def t_04():
+  x = {'value': None}
+  y = ' '
+  z = f(make_cell(**x))
+  return pxyz(x, y, z)
+
+def t_05():
+  x = {'value': 1.0}
+  y = '1.00'
+  z = f(make_cell(**x))
+  return pxyz(x, y, z)
+
+def t_06():
+  x = {'value': make_rate(710, 113, {'a': 1})}
+  y = '6.28'
+  z = f(make_cell(**x))
+  return pxyz(x, y, z)
+
 def t():
   if not t_a(): return pf('t_a failed')
   if not t_0(): return pf('t_0 failed')
   if not t_10(): return pf('t_10 failed')
+  if not t_01(): return pf('t_01 failed')
+  if not t_02(): return pf('t_02 failed')
+  if not t_03(): return pf('t_03 failed')
+  if not t_04(): return pf('t_04 failed')
+  if not t_05(): return pf('t_05 failed')
+  if not t_06(): return pf('t_06 failed')
   return True
