551f9ede66b7bcc053d656e668e82f295e16371c
John Forbes <john.robert.forbes@gmail.com>
2023-08-14 18:17:36 +1000

    Updated src/column/width/get.py.

--- src/column/width/get.py
+++ src/column/width/get.py
@@ -1,24 +1,25 @@
 from hak.pf import f as pf
 from hak.pxyz import f as pxyz
 from src.column.make import f as make_column
+from src.cell.make import f as make_cell
 
 # width
-f = lambda x: max(len(x['name']), *[len(str(v)) for v in x['values']])
+f = lambda x: max(len(x['name']), *[len(str(c['value'])) for c in x['cells']])
 
 def t_a():
-  x = make_column('apples', [_ for _ in range(100)])
+  x = make_column('apples', [make_cell(_, 'apples') for _ in range(100)])
   y = 6
   z = f(x)
   return pxyz(x, y, z)
 
-def t_values_dominant():
-  x = make_column('a', [0, 1000, 2, 3])
+def t_cells_dominant():
+  x = make_column('a', [make_cell(v, 'a') for v in [0, 1000, 2, 3]])
   y = 4
   z = f(x)
   return pxyz(x, y, z)
 
 def t_name_dominant():
-  x = make_column('abc', [0, 1, 2, 3])
+  x = make_column('abc', [make_cell(v, 'abc') for v in [0, 1, 2, 3]])
   y = 3
   z = f(x)
   return pxyz(x, y, z)
@@ -26,5 +27,5 @@ def t_name_dominant():
 def t():
   if not t_a(): return pf('!t_a')
   if not t_name_dominant(): return pf('!t_name_dominant')
-  if not t_values_dominant(): return pf('!t_values_dominant')
+  if not t_cells_dominant(): return pf('!t_cells_dominant')
   return True
