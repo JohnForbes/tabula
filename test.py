c8cacda7f48e007e990c58ab92280e9c7f4d1d15
John Forbes <john.robert.forbes@gmail.com>
2023-08-14 13:49:14 +1000

    Updated test.py.

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from hak.one.directory.clean import f as clean_directory
 from hak.test.do import f as do_test
 from hak.test.final_line.check import f as check_final_line
 from hak.test.line_lengths.check import f as check_line_lengths
@@ -5,6 +6,7 @@ from hak.test.oldest_file.print import f as print_oldest_file
 
 if __name__ == '__main__':
   print('|'+'-'*78+'|')
+  clean_directory('.')
   z = do_test(False)
   print(z['message'])
   if z['result']:
