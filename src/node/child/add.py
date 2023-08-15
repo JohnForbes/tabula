9f905cad1ddf5603694c9b6f297c39cd455c7981
John Forbes <john.robert.forbes@gmail.com>
2023-08-15 17:22:41 +1000

    Added src/node/child/add.py.

---
+++ src/node/child/add.py
@@ -0,0 +1,20 @@
+from hak.pf import f as pf
+from hak.one.list.append_if_not_present import f as append_if_not_present
+
+from src.node.make import f as make_node
+
+# add_child
+def f(node, child):
+  node['children'] = append_if_not_present(node['children'], child)
+  child['parent'] = node
+  return node, child
+
+def t():
+  node = make_node('foo')
+  child = make_node('bar')
+  f(node, child)
+  if not len(node['children']): return pf("not len(x['node']['children'])")
+  if not (node['children'][0]['name'] == 'bar'): return pf('!A')
+  if not (child['parent']['name'] == 'foo'): return pf('!B')
+  if not (child['parent']['children'][0]['name'] == 'bar'): return pf('!C')
+  return True
