f1bd0d9820cf7629b3810463cd89b458dccf62be
John Forbes <john.robert.forbes@gmail.com>
2023-08-15 11:34:47 +1000

    Updated src/column/to_str.py.

--- src/column/to_str.py
+++ src/column/to_str.py
@@ -6,11 +6,24 @@ from hak.pxyz import f as pxyz
 from src.cell.to_str import f as cell_to_str
 from src.column.make_from_values import f as make_column
 from src.column.width.get import f as get_width
+from hak.one.dict.unit.to_str import f as unit_to_str
 
 def f(x):
   _w = get_width(x)
   w = _w + 2
   cell_strings = [f" {cell_to_str(c):>{_w}} " for c in x['cells']]
+  
+  if x['cells'][0]['datatype'] == 'rate':
+    return '\n'.join([
+      '-'*w,
+      f" {x['name']:>{_w}} ",
+      '-'*w,
+      f" {unit_to_str(x['cells'][0]['value']['unit']):>{_w}} ",
+      '-'*w,
+      *cell_strings,
+      '-'*w,
+    ])
+
   return '\n'.join([
     '-'*w,
     f" {x['name']:>{_w}} ",
@@ -138,10 +151,36 @@ def t_rate():
     '------',
     ' rate ',
     '------',
+    '    m ',
+    '------',
     '  0/1 ',
     '  1/9 ',
     '  1/4 ',
     '------',
+  ])
+  z = f(x)
+  return pxyz(x, y, z, new_line=1)
+
+def t_rate_long_unit():
+  _unit = {'$': 1, 'banana': -1}
+  x = make_column(
+    'rate',
+    [
+      make_rate(0, 10, _unit),
+      make_rate(1,  9, _unit),
+      make_rate(2,  8, _unit)
+    ]
+  )
+  y = '\n'.join([
+    '----------',
+    '     rate ',
+    '----------',
+    ' $/banana ',
+    '----------',
+    '      0/1 ',
+    '      1/9 ',
+    '      1/4 ',
+    '----------',
   ])
   z = f(x)
   return pxyz(x, y, z, new_line=1)
@@ -155,4 +194,5 @@ def t():
   if not t_date(): return pf('!t_date')
   if not t_str(): return pf('!t_str')
   if not t_rate(): return pf('!t_rate')
+  if not t_rate_long_unit(): return pf('!t_rate_long_unit')
   return True
