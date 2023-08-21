c659d15edfb5059b54c25d6ce992a78a3eabb506
John Forbes <john.robert.forbes@gmail.com>
2023-08-21 15:37:20 +1000

    Removed src/node/make.py.

--- src/node/make.py
+++
@@ -1,9 +0,0 @@
-from hak.pxyz import f as pxyz
-
-f = lambda name: {'name': name, 'value': None, 'parent': None, 'children': []}
-
-def t():
-  x = 'foo'
-  y = {'name': 'foo', 'value': None, 'parent': None, 'children': []}
-  z = f(x)
-  return pxyz(x, y, z)
