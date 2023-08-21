c293662a466e35bca6acfe37ecf1d7416fd51765
John Forbes <john.robert.forbes@gmail.com>
2023-08-21 15:34:35 +1000

    Updated test.py.

--- test.py
+++ test.py
@@ -3,12 +3,9 @@ from hak.test.do import f as do_test
 from hak.test.final_line.check import f as check_final_line
 from hak.test.line_lengths.check import f as check_line_lengths
 from hak.test.oldest_file.print import f as print_oldest_file
-from hak.one.directory.filepaths.get import f as get_filepaths
-from hak.many.strings.filepaths.get_least_recently_modified import f as get_least_recently_modified
 
 if __name__ == '__main__':
   print('|'+'-'*78+'|')
-  clean_directory('.')
   z = do_test(False)
   print(z['message'])
   if z['result']:
@@ -16,6 +13,5 @@ if __name__ == '__main__':
     check_final_line()
     print_oldest_file()
   
-  archived = get_filepaths('./archive', [])
-  print(f'Oldest archived: {get_least_recently_modified(archived)}')
   print('|'+'-'*78+'|')
+  clean_directory('.')
