efc5cf7d43904d9ba8da0961de59a4489c85b222
John Forbes <john.robert.forbes@gmail.com>
2023-08-21 15:40:46 +1000

    Updated src/main.py.

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ def f(x):
   if len(x[0]) < 1: return ''
   k = list(x[0].keys())[0]
   v = x[0][k]
-  w = len(str(v))
+  w = max([len(str(k)), len(str(v))])
   b = make_homogenous_line('-', w)
   row_strings = [
     b,
@@ -67,10 +67,23 @@ def t_a_10():
   z = f(x)
   return pxyz(x, y, z, new_line=1)
 
+def t_aaa_10():
+  x = [{'aaa': 10}]
+  y = '\n'.join([
+    '-----',
+    ' aaa ',
+    '-----',
+    '  10 ',
+    '-----'
+  ])
+  z = f(x)
+  return pxyz(x, y, z, new_line=1)
+
 def t():
   if not t_0(): return pf('!t_0')
   if not t_1(): return pf('!t_1')
   if not t_a_0(): return pf('!t_a_0')
   if not t_b_0(): return pf('!t_b_0')
   if not t_a_10(): return pf('!t_a_10')
+  if not t_aaa_10(): return pf('!t_aaa_10')
   return True
