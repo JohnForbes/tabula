b7018837139bbee3d34dcd2fc27cf3503f97d0f2
John Forbes <john.robert.forbes@gmail.com>
2023-08-21 20:46:35 +1000

    Added src/functions/dict/cell/is_a.py.

---
+++ src/functions/dict/cell/is_a.py
@@ -0,0 +1,98 @@
+from hak.one.bool.random.make import f as make_random_bool
+from hak.one.dict.is_a import f as is_dict
+from hak.one.list.random.make import f as make_random_list
+from hak.one.number.float.random.make import f as make_random_float
+from hak.one.number.int.random.make import f as make_random_int
+from hak.one.set.random.make import f as make_random_set
+from hak.one.string.random.make import f as make_random_str
+from hak.one.tuple.random.make import f as make_random_tuple
+from hak.pf import f as pf
+from hak.pxyz import f as pxyz
+
+from src.functions.dict.cell.make import f as make_cell
+
+def f(x):
+  if not is_dict(x): return False
+  if len(x) != 3: return False
+  if 'value' not in x: return False
+  return True
+
+def t_false_none():
+  x = None
+  y = False
+  z = f(x)
+  return pxyz(x, y, z)
+
+def t_false_int():
+  x = make_random_int(0, 10)
+  y = False
+  z = f(x)
+  return pxyz(x, y, z)
+
+def t_false_float():
+  x = make_random_float()
+  y = False
+  z = f(x)
+  return pxyz(x, y, z)
+
+def t_false_str():
+  x = make_random_str()
+  y = False
+  z = f(x)
+  return pxyz(x, y, z)
+
+def t_false_set():
+  x = make_random_set()
+  y = False
+  z = f(x)
+  return pxyz(x, y, z)
+
+def t_false_tuple():
+  x = make_random_tuple()
+  y = False
+  z = f(x)
+  return pxyz(x, y, z)
+
+def t_false_bool():
+  x = make_random_bool()
+  y = False
+  z = f(x)
+  return pxyz(x, y, z)
+
+def t_false_list():
+  x = make_random_list()
+  y = False
+  z = f(x)
+  return pxyz(x, y, z)
+
+def t_false_dict_empty():
+  x = {}
+  y = False
+  z = f(x)
+  return pxyz(x, y, z)
+
+def t_false_dict_wrong_k_count():
+  x = {'value': 0}
+  y = False
+  z = f(x)
+  return pxyz(x, y, z)
+
+def t_true():
+  x = make_cell(value=0, field_name='i')
+  y = True
+  z = f(x)
+  return pxyz(x, y, z)
+
+def t():
+  if not t_false_bool(): return pf('!t_false_bool')
+  if not t_false_none(): return pf('!t_false_none')
+  if not t_false_int(): return pf('!t_false_int')
+  if not t_false_float(): return pf('!t_false_float')
+  if not t_false_str(): return pf('!t_false_str')
+  if not t_false_set(): return pf('!t_false_set')
+  if not t_false_tuple(): return pf('!t_false_tuple')
+  if not t_false_list(): return pf('!t_false_list')
+  if not t_false_dict_empty(): return pf('!t_false_dict_empty')
+  if not t_false_dict_wrong_k_count(): return pf('!t_false_dict_wrong_k_count')
+  if not t_true(): return pf('!t_true')
+  return True
