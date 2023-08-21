42362c66f5874717ccc765a75ebf2ae0587d7470
John Forbes <john.robert.forbes@gmail.com>
2023-08-21 22:15:39 +1000

    Added src/functions/strings/cell_lines/to_row_line.py.

---
+++ src/functions/strings/cell_lines/to_row_line.py
@@ -0,0 +1,12 @@
+# from archive.strings.to_table_row import f as cell_strings_to_table_row_string
+from ..cell_strings.to_table_row import f as cell_strings_to_table_row_string
+from hak.pxyz import f as pxyz
+
+# cell_lines_to_row_line
+f = lambda x: cell_strings_to_table_row_string(x, '-')
+
+def t():
+  x = ['-------', '--------', '--------', '---------', '----------', '------']
+  y = '|---------|----------|----------|-----------|------------|--------|'
+  z = f(x)
+  return pxyz(x, y, z)
