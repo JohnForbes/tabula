91dbebc0f02e22932e6779086db7ff0799770fb3
John Forbes <john.robert.forbes@gmail.com>
2023-08-22 10:43:04 +1000

    Added src/functions/string/table/add_right_border.py.

---
+++ src/functions/string/table/add_right_border.py
@@ -0,0 +1,25 @@
+from hak.pxyz import f as pxyz
+
+f = lambda x: '\n'.join([_x_i+'|' for _x_i in x.split('\n')])
+
+def t():
+  x = '\n'.join([
+    '---|---',
+    ' a | b ',
+    '---|---',
+    ' 0 | 1 ',
+    ' 3 | 4 ',
+    ' 6 | 7 ',
+    '---|---',
+  ])
+  y = '\n'.join([
+    '---|---|',
+    ' a | b |',
+    '---|---|',
+    ' 0 | 1 |',
+    ' 3 | 4 |',
+    ' 6 | 7 |',
+    '---|---|',
+  ])
+  z = f(x)
+  return pxyz(x, y, z, new_line=1)
