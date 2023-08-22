dc5c41b891b239c0137c0ffef4a3a886f55c9fc2
John Forbes <john.robert.forbes@gmail.com>
2023-08-22 15:46:22 +1000

    Updated src/functions/dict/value_and_width/to_str.py.

--- src/functions/dict/value_and_width/to_str.py
+++ src/functions/dict/value_and_width/to_str.py
@@ -4,7 +4,7 @@ from hak.pf import f as pf
 # g
 # f = lambda v, w: f' {v:>{w}} '
 # def f(v, w): return f' {str(v):>{w}} '
-f = lambda x: f" {x['value']:>{x['width']}} "
+f = lambda x: f" {str(x['value']):>{x['width']}} "
 
 def t_int():
   x = {'value': 12, 'width': 10}
