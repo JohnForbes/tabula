d9b9969816914fcc2f9b13b49288f14df65aebe2
John Forbes <john.robert.forbes@gmail.com>
2023-08-23 22:14:09 +1000

    Updated src/functions/dict/value_and_width/to_str.py.

--- src/functions/dict/value_and_width/to_str.py
+++ src/functions/dict/value_and_width/to_str.py
@@ -1,17 +1,13 @@
-from hak.pxyz import f as pxyz
 from hak.pf import f as pf
+from hak.pxyf import f as pxyf
 
 # g
 # f = lambda v, w: f' {v:>{w}} '
 # def f(v, w): return f' {str(v):>{w}} '
 f = lambda x: f" {str(x['value']):>{x['width']}} "
 
-def t_int():
-  x = {'value': 12, 'width': 10}
-  y = '         12 '
-  z = f(x)
-  return pxyz(x, y, z)
+t_int = lambda: pxyf({'value': 12, 'width': 10}, '         12 ', f)
 
 def t():
   if not t_int(): return pf('!t_int')
-  return True
+  return 1
