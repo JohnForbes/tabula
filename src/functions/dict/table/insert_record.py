64daff7faf5c444ce2f21c5d3f1080a280444de3
John Forbes <john.robert.forbes@gmail.com>
2023-08-23 22:12:51 +1000

    Updated src/functions/dict/table/insert_record.py.

--- src/functions/dict/table/insert_record.py
+++ src/functions/dict/table/insert_record.py
@@ -1,57 +1,50 @@
 from hak.one.list.append_if_not_present import f as append_if_not_present
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
-from src.functions.dict.cell.make import f as make_cell
-from src.functions.dict.table.make import f as make_table
+from ..cell.make import f as cell
+from .make import f as table
 
-def f(table, record):
-  max_row_identifier = max(table['row_order']) if table['row_order'] else -1
+def f(x):
+  _table = x['table']
+  _record = x['record']
+  max_row_identifier = max(_table['row_order']) if _table['row_order'] else -1
   row_identifier = max_row_identifier+1
-  table['row_order'] = table['row_order'] + [row_identifier]
+  _table['row_order'] = _table['row_order'] + [row_identifier]
 
-  for (k, v) in record.items():
-    table['column_order'] = append_if_not_present(table['column_order'], k)
-    table['cells'][(k, row_identifier)] = make_cell(v, k)
+  for (k, v) in _record.items():
+    _table['column_order'] = append_if_not_present(_table['column_order'], k)
+    _table['cells'][(k, row_identifier)] = cell({'value': v, 'field_name': k})
 
-  return table
+  return _table
 
 def t_flat():
-  x = {'table': make_table(), 'record': {'a': 0, 'b': 1, 'c': 2}}
+  x = {'table': table(), 'record': {'a': 0, 'b': 1, 'c': 2}}
   y = {
     'column_order': ['a', 'b', 'c'],
     'row_order': [0],
     'cells': {
-      ('a', 0): make_cell(0, 'a'),
-      ('b', 0): make_cell(1, 'b'),
-      ('c', 0): make_cell(2, 'c')
+      ('a', 0): cell({'value': 0, 'field_name': 'a'}),
+      ('b', 0): cell({'value': 1, 'field_name': 'b'}),
+      ('c', 0): cell({'value': 2, 'field_name': 'c'})
     }
   }
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyf(x, y, f)
 
 def t_nested():
-  x = {
-    'table': make_table(),
-    'record': {
-      'prices': {
-        'a': 0, 'b': 1, 'c': 2
-      }
-    }
-  }
+  x = {'table': table(), 'record': {'prices': {'a': 0, 'b': 1, 'c': 2}}}
   y = {
     'column_order': ['a', 'b', 'c'],
     'row_order': [0],
     'cells': {
-      ('a', 0): make_cell(0, 'a'),
-      ('b', 0): make_cell(1, 'b'),
-      ('c', 0): make_cell(2, 'c')
+      ('a', 0): cell({'value': 0, 'field_name': 'a'}),
+      ('b', 0): cell({'value': 1, 'field_name': 'b'}),
+      ('c', 0): cell({'value': 2, 'field_name': 'c'})
     }
   }
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyf(x, y, f)
 
 def t():
   if not t_flat(): return pf('!t_flat')
-  # if not t_nested(): return pf('!t_nested')
-  return True
+  # if not t_nested(): return pf('!t_nested') # TODO
+  return 1
