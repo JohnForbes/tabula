c79b2ec61fc724f7e4b0c556b35646bbd8431a0a
John Forbes <john.robert.forbes@gmail.com>
2023-08-23 11:22:42 +1000

    Updated src/functions/dict/record_and_keypath/to_str.py.

--- src/functions/dict/record_and_keypath/to_str.py
+++ src/functions/dict/record_and_keypath/to_str.py
@@ -1,18 +1,24 @@
-from datetime import date
 from hak.one.dict.rate.make import f as make_rate
+from hak.pf import f as pf
 from hak.pxyz import f as pxyz
+from datetime import date
 
 from src.functions.dict.column.make_from_values import f as make_from_values
 from src.functions.dict.column.to_str import f as column_to_str
 
-def f(x):
+def f_dates_date(x):
   keypath = x['keypath']
   k_0 = keypath[0] # 'dates'
   k_1 = keypath[1] # 'date'
   column = make_from_values(k_1, [x['record'][k_0][k_1]], k_0)
   return column_to_str(column)
 
-def t():
+def f(x):
+  # if x['keypath'] == ('dates', 'date'): return f_dates_date(x)
+  # return f_info_not_header(x)
+  return f_dates_date(x)
+
+def t_date():
   x = {
     'record': {
       'dates': {'date': date(2023, 1, 1)},
@@ -35,5 +41,6 @@ def t():
   z = f(x)
   return pxyz(x, y, z, new_line=1)
 
-if __name__ == '__main__':
-  print(t())
+def t():
+  if not t_date(): return pf('!t_date')
+  return True
