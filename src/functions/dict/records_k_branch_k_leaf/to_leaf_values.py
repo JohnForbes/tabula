95c79e8728dc48c171193934352c0280263e4c0a
John Forbes <john.robert.forbes@gmail.com>
2023-08-23 21:40:43 +1000

    Updated src/functions/dict/records_k_branch_k_leaf/to_leaf_values.py.

--- src/functions/dict/records_k_branch_k_leaf/to_leaf_values.py
+++ src/functions/dict/records_k_branch_k_leaf/to_leaf_values.py
@@ -1,6 +1,6 @@
 from hak.one.dict.rate.to_str import f as rate_to_str
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 from hak.one.dict.rate.make import f as make_rate
 
 # records_k_branch_k_leaf_to_leaf_values
@@ -38,8 +38,7 @@ def t_prices_apples():
     'k_leaf':   'apples'
   }
   y = ['0.25', '0.75']
-  z = f(x)
-  return pxyz(x, y, z)
+  return pxyf(x, y, f)
 
 def t_prices_bananas():
   x = {
@@ -73,8 +72,7 @@ def t_prices_bananas():
     'k_leaf':   'bananas'
   }
   y = ['0.50', '1.00']
-  z = f(x)
-  return pxyz(x, y, z)
+  return pxyf(x, y, f)
 
 def t_volumes_applezzz():
   x = {
@@ -108,8 +106,7 @@ def t_volumes_applezzz():
     'k_leaf':   'applezzz'
   }
   y = ['1.00', '4.00']
-  z = f(x)
-  return pxyz(x, y, z)
+  return pxyf(x, y, f)
 
 def t_volumes_bananazzz():
   x = {
@@ -143,8 +140,7 @@ def t_volumes_bananazzz():
     'k_leaf':   'bananazzz'
   }
   y = ['2.00', '5.00']
-  z = f(x)
-  return pxyz(x, y, z)
+  return pxyf(x, y, f)
 
 def t_volumes_pearzzzzzz():
   x = {
@@ -178,8 +174,7 @@ def t_volumes_pearzzzzzz():
     'k_leaf':   'pearzzzzzz'
   }
   y = ['3.00', '6.00']
-  z = f(x)
-  return pxyz(x, y, z)
+  return pxyf(x, y, f)
 
 def t_zloops_zloop():
   x = {
@@ -213,8 +208,7 @@ def t_zloops_zloop():
     'k_leaf':   'zloop'
   }
   y = ['7.00', '7.00']
-  z = f(x)
-  return pxyz(x, y, z)
+  return pxyf(x, y, f)
 
 def t():
   if not t_prices_apples(): return pf('!t_prices_apples')
@@ -223,4 +217,4 @@ def t():
   if not t_volumes_bananazzz(): return pf('!t_volumes_bananazzz')
   if not t_volumes_pearzzzzzz(): return pf('!t_volumes_pearzzzzzz')
   if not t_zloops_zloop(): return pf('!t_zloops_zloop')
-  return True
+  return 1
