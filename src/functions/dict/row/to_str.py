7c6d9a9675e2970417441d45c2423bf83e4d733d
John Forbes <john.robert.forbes@gmail.com>
2023-08-23 21:51:10 +1000

    Updated src/functions/dict/row/to_str.py.

--- src/functions/dict/row/to_str.py
+++ src/functions/dict/row/to_str.py
@@ -1,31 +1,20 @@
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 from src.functions.dict.cell.make import f as make_cell
 from src.functions.dict.cell.to_str import f as cell_to_str
 
 f = lambda x: '| '+' | '.join([cell_to_str(x[k]) for k in x])+' |'
 
-def t_0():
-  x = {'a': make_cell(1, 'a'), 'b': make_cell(2, 'b'), 'c': make_cell(3, 'c')}
+def t_a():
+  x = {
+    'a': make_cell({'value': 1, 'field_name': 'a'}),
+    'b': make_cell({'value': 2, 'field_name': 'b'}),
+    'c': make_cell({'value': 3, 'field_name': 'c'})
+  }
   y = '| 1 | 2 | 3 |'
-  z = f(x)
-  return pxyz(x, y, z)
-
-# def t_a():
-#   x = ['   0.25', '    0.50', '    1.00', '     2.00', '      3.00', '  7.00']
-#   y = '|    0.25 |     0.50 |     1.00 |      2.00 |       3.00 |   7.00 |'
-#   z = f(x)
-#   return pxyz(x, y, z)
-
-# def t_b():
-#   x = ['   0.75', '    1.00', '    4.00', '     5.00', '      6.00', '  7.00']
-#   y = '|    0.75 |     1.00 |     4.00 |      5.00 |       6.00 |   7.00 |'
-#   z = f(x)
-#   return pxyz(x, y, z)
+  return pxyf(x, y, f)
 
 def t():
-  if not t_0(): return pf('!t_0')
-  # if not t_a(): return pf('!t_a')
-  # if not t_b(): return pf('!t_b')
-  return True
+  if not t_a(): return pf('!t_a')
+  return 1
