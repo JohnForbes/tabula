8a466c7343331c8e4337a1f73b47d9b6d89b3af2
John Forbes <john.robert.forbes@gmail.com>
2023-08-23 16:29:56 +1000

    Updated src/functions/dict/records_and_index/to_padded_cell_values_of_col_width.py.

--- src/functions/dict/records_and_index/to_padded_cell_values_of_col_width.py
+++ src/functions/dict/records_and_index/to_padded_cell_values_of_col_width.py
@@ -1,59 +1,33 @@
 # ignore_overlength_lines
-from hak.one.dict.rate.make import f as make_rate
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
+from hak.many.dicts.a_into_b import f as a_into_b
 
-from ..records_k_branch_k_leaf_index.to_cell_str import f as g
-from ..records_k_branch_k_leaf.to_k_branch_k_leaf_pairs import f as h
+from ..records_k_branch_k_leaf_index.to_cell_str import f as fn_a
+from ..records_k_branch_k_leaf.to_k_branch_k_leaf_pairs import f as fn_b
+from data.records import records_without_date as _records
 
 # f_y
 # records_and_index_to_padded_cell_values_of_col_width
-f = lambda records, index: [
-  g({'records': records, 'k_branch': k_branch, 'k_leaf': k_leaf, 'index': index})
-  for (k_branch, k_leaf)
-  in h(records)
+f = lambda x: [
+  fn_a(a_into_b({'k_branch': k_b, 'k_leaf': k_l}, x))
+  for (k_b, k_l)
+  in fn_b(x['records'])
 ]
 
-_records = [
-  {
-    'prices': {
-      'apples': make_rate(1, 4, {'$': 1, 'apple': -1}),
-      'bananas': make_rate(1, 2, {'$': 1, 'banana': -1})
-    },
-    'volumes': {
-      'applezzz': make_rate(1, 1, {'apple': 1}),
-      'bananazzz': make_rate(2, 1, {'banana': 1}),
-      'pearzzzzzz': make_rate(3, 1, {'pear': 1})
-    },
-    'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-  }, 
-  {
-    'prices': {
-      'apples': make_rate(3, 4, {'$': 1, 'apple': -1}),
-      'bananas': make_rate(1, 1, {'$': 1, 'banana': -1})
-    },
-    'volumes': {
-      'applezzz': make_rate(4, 1, {'apple': 1}),
-      'bananazzz': make_rate(5, 1, {'banana': 1}),
-      'pearzzzzzz': make_rate(6, 1, {'pear': 1})
-    },
-    'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-  }
-]
+t_0 = lambda: pxyf(
+  {'records': _records, 'index': 0},
+  ['   0.25', '    0.50', '    1.00', '     2.00', '      3.00', '  7.00'],
+  f
+)
 
-def t_0():
-  x = {'records': _records, 'index': 0}
-  y = ['   0.25', '    0.50', '    1.00', '     2.00', '      3.00', '  7.00']
-  z = f(**x)
-  return pxyz(x, y, z)
-
-def t_1():
-  x = {'records': _records, 'index': 1}
-  y = ['   0.75', '    1.00', '    4.00', '     5.00', '      6.00', '  7.00']
-  z = f(**x)
-  return pxyz(x, y, z)
+t_1 = lambda: pxyf(
+  {'records': _records, 'index': 1},
+  ['   0.75', '    1.00', '    4.00', '     5.00', '      6.00', '  7.00'],
+  f
+)
 
 def t():
   if not t_0(): return pf('!t_0')
   if not t_1(): return pf('!t_1')
-  return True
+  return 1
