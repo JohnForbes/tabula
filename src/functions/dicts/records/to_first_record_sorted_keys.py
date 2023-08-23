2be9a02b1c92c7242e807b022c24cf7b34b0f14a
John Forbes <john.robert.forbes@gmail.com>
2023-08-23 22:17:33 +1000

    Updated src/functions/dicts/records/to_first_record_sorted_keys.py.

--- src/functions/dicts/records/to_first_record_sorted_keys.py
+++ src/functions/dicts/records/to_first_record_sorted_keys.py
@@ -1,11 +1,7 @@
 from hak.one.dict.get_sorted_keys import f as get_sorted_keys
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 # records_to_first_record_sorted_keys
 f = lambda x: get_sorted_keys(x[0])
 
-def t():
-  x = [{'c': 0, 'b': 1, 'a': 2}, {}]
-  y = list('abc')
-  z = f(x)
-  return pxyz(x, y, z)
+t = lambda: pxyf([{'c': 0, 'b': 1, 'a': 2}, {}], list('abc'), f)
