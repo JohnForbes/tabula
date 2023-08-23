e999bcd244f75c9e70136716fb0444790bb8da9c
John Forbes <john.robert.forbes@gmail.com>
2023-08-23 15:19:09 +1000

    Updated src/functions/dict/records_and_field_name/to_pad_k_branch.py.

--- src/functions/dict/records_and_field_name/to_pad_k_branch.py
+++ src/functions/dict/records_and_field_name/to_pad_k_branch.py
@@ -1,7 +1,7 @@
 # ignore_overlength_lines
 from datetime import date
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 from .to_sorted_leaf_keys import f as records_k_branch_to_sorted_leaf_keys
 from ..records_k_branch_k_leaf.to_leaf_col_width import f as records_k_branch_k_leaf_to_leaf_col_width
@@ -51,23 +51,23 @@ _records = [
   }
 ]
 
-def t_prices():
-  x = {'records': _records, 'field_name': 'prices'}
-  y = '            prices'
-  z = f(x)
-  return pxyz(x, y, z)
+t_prices = lambda: pxyf(
+  {'records': _records, 'field_name': 'prices'},
+  '            prices',
+  f
+)
 
-def t_volumes():
-  x = {'records': _records, 'field_name': 'volumes'}
-  y = '                          volumes'
-  z = f(x)
-  return pxyz(x, y, z)
+t_volumes = lambda: pxyf(
+  {'records': _records, 'field_name': 'volumes'},
+  '                          volumes',
+  f
+)
 
-def t_zloops():
-  x = {'records': _records, 'field_name': 'zloops'}
-  y = 'zloops'
-  z = f(x)
-  return pxyz(x, y, z)
+t_zloops = lambda: pxyf(
+  {'records': _records, 'field_name': 'zloops'},
+  'zloops',
+  f
+)
 
 def t_date():
   x = {
@@ -75,39 +75,37 @@ def t_date():
       {
         'date': date(2023, 7, 27),
         'prices': {
-          'apples': {'numerator': 1, 'denominator': 4, 'unit': {'$': 1, 'apple': -1}},
-          'bananas': {'numerator': 1, 'denominator': 2, 'unit': {'$': 1, 'banana': -1}}
+          'apples': make_rate(1, 4, {'$': 1, 'apple': -1}),
+          'bananas': make_rate(1, 2, {'$': 1, 'banana': -1})
         },
         'volumes': {
-          'applezzz': {'numerator': 1, 'denominator': 1, 'unit': {'apple': 1}},
-          'bananazzz': {'numerator': 2, 'denominator': 1, 'unit': {'banana': 1}},
-          'pearzzzzzz': {'numerator': 3, 'denominator': 1, 'unit': {'pear': 1}}
+          'applezzz': make_rate(1, 1, {'apple': 1}),
+          'bananazzz': make_rate(2, 1, {'banana': 1}),
+          'pearzzzzzz': make_rate(3, 1, {'pear': 1})
         },
-        'zloops': {'zloop': {'numerator': 7, 'denominator': 1, 'unit': {'zloop': 1}}}
+        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
       },
       {
         'date': date(2023, 7, 28),
         'prices': {
-          'apples': {'numerator': 3, 'denominator': 4, 'unit': {'$': 1, 'apple': -1}},
-          'bananas': {'numerator': 1, 'denominator': 1, 'unit': {'$': 1, 'banana': -1}}
+          'apples': make_rate(3, 4, {'$': 1, 'apple': -1}),
+          'bananas': make_rate(1, 1, {'$': 1, 'banana': -1})
         },
         'volumes': {
-          'applezzz': {'numerator': 4, 'denominator': 1, 'unit': {'apple': 1}},
-          'bananazzz': {'numerator': 5, 'denominator': 1, 'unit': {'banana': 1}},
-          'pearzzzzzz': {'numerator': 6, 'denominator': 1, 'unit': {'pear': 1}}
+          'applezzz': make_rate(4, 1, {'apple': 1}),
+          'bananazzz': make_rate(5, 1, {'banana': 1}),
+          'pearzzzzzz': make_rate(6, 1, {'pear': 1})
         },
-        'zloops': {'zloop': {'numerator': 7, 'denominator': 1, 'unit': {'zloop': 1}}}
+        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
       }
     ],
     'field_name': 'date'
   }
-  y = 10
-  z = f(x)
-  return pxyz(x, y, z)
+  return pxyf(x, 10, f)
 
 def t():
-  # if not t_date(): return pf('!t_date')
+  # if not t_date(): return pf('!t_date') # TODO
   if not t_prices(): return pf('!t_prices')
   if not t_volumes(): return pf('!t_volumes')
   if not t_zloops(): return pf('!t_zloops')
-  return True
+  return 1
