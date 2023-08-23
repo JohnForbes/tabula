bd355806043db42f0aaaea9f22899e5fc07e1900
John Forbes <john.robert.forbes@gmail.com>
2023-08-23 23:50:53 +1000

    Updated src/functions/ints/cell_value_widths/to_aggregate_width.py.

--- src/functions/ints/cell_value_widths/to_aggregate_width.py
+++ src/functions/ints/cell_value_widths/to_aggregate_width.py
@@ -1,29 +1,29 @@
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 # cell_val_widths_to_aggregate_width
 f = lambda x: sum(x)+(len(x)-1)*len(' | ')
 
-def t_6():
-  x = [6] # | 123456 |
-  y = 6   # | 123456 |
-  z = f(x)
-  return pxyz(x, y, z)
+t_6 = lambda: pxyf(
+  [6], # | 123456 |
+  6,   # | 123456 |
+  f
+)
 
-def t_7_8():
-  x = [7, 8] # | 1234567 | 12345678 |
-  y = 18     # | 123456789012345678 |
-  z = f(x)
-  return pxyz(x, y, z)
+t_7_8 = lambda: pxyf(
+  [7, 8], # | 1234567 | 12345678 |
+  18,     # | 123456789012345678 |
+  f
+)
 
-def t_8_9_10():
-  x = [8, 9, 10] # | 12345678 | 123456789 | 1234567890 |
-  y = 33         # | 123456789012345678901234567890123 |
-  z = f(x)
-  return pxyz(x, y, z)
+t_8_9_10 = lambda: pxyf(
+  [8, 9, 10], # | 12345678 | 123456789 | 1234567890 |
+  33,         # | 123456789012345678901234567890123 |
+  f
+)
 
 def t():
   if not t_6():      return pf('!t_6')
   if not t_7_8():    return pf('!t_7_8')
   if not t_8_9_10(): return pf('!t_8_9_10')
-  return True
+  return 1
