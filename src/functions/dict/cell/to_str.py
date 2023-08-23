6d8f5d7869c55abc791ec0cb9f8b2d331bb9a724
John Forbes <john.robert.forbes@gmail.com>
2023-08-23 12:41:02 +1000

    Updated src/functions/dict/cell/to_str.py.

--- src/functions/dict/cell/to_str.py
+++ src/functions/dict/cell/to_str.py
@@ -8,7 +8,7 @@ from hak.one.number.float.is_a import f as is_float
 from hak.one.string.colour.bright.green import f as g
 from hak.one.string.colour.bright.red import f as r
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 from src.functions.dict.cell.make import f as make_cell
 
@@ -33,37 +33,18 @@ def f(x):
 #   _width = v_or_0(x, 'width')
 #   return ' '*_width + f'{_val_str:>{_width}}'
 
-def t_a():
-  x = make_cell(**{'value': 'a', 'field_name': 'A'})
-  return pxyz(x, 'a', f(x))
-
-def t_0():
-  x = make_cell(**{'value': 0, 'field_name': 'i'})
-  return pxyz(x, ' ', f(x))
-
-def t_10():
-  x = make_cell(**{'value': 10, 'field_name': 'i'})
-  return pxyz(x, '10', f(x))
-
-def t_02():
-  x = make_cell(**{'value': False, 'field_name': 'B'})
-  return pxyz(x, r('N'), f(x))
-
-def t_03():
-  x = make_cell(**{'value': True, 'field_name': 'B'})
-  return pxyz(x, g('Y'), f(x))
-
-def t_04():
-  x = make_cell(**{'value': None, 'field_name': 'foo'})
-  return pxyz(x, ' ', f(x))
-
-def t_05():
-  x = make_cell(**{'value': 1.0, 'field_name': 'foo'})
-  return pxyz(x, '1.00', f(x))
-
-def t_06():
-  x = make_cell(**{'value': make_rate(710, 113, {'a': 1}), 'field_name': 'foo'})
-  return pxyz(x, '710/113', f(x))
+t_a = lambda: pxyf(make_cell({'value': 'a', 'field_name': 'A'}), 'a', f)
+t_0 = lambda: pxyf(make_cell({'value': 0, 'field_name': 'i'}), ' ', f)
+t_10 = lambda: pxyf(make_cell({'value': 10, 'field_name': 'i'}), '10', f)
+t_02 = lambda: pxyf(make_cell({'value': False, 'field_name': 'B'}), r('N'), f)
+t_03 = lambda: pxyf(make_cell({'value': True, 'field_name': 'B'}), g('Y'), f)
+t_04 = lambda: pxyf(make_cell({'value': None, 'field_name': 'foo'}), ' ', f)
+t_05 = lambda: pxyf(make_cell({'value': 1.0, 'field_name': 'foo'}), '1.00', f)
+t_06 = lambda: pxyf(
+  make_cell({'value': make_rate(710, 113, {'a': 1}), 'field_name': 'foo'}),
+  '710/113',
+  f
+)
 
 def t():
   if not t_a(): return pf('t_a failed')
