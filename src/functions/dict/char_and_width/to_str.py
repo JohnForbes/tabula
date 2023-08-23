5a8de8022e36147acf5eac300be9a71cf76f0d1b
John Forbes <john.robert.forbes@gmail.com>
2023-08-23 12:53:09 +1000

    Updated src/functions/dict/char_and_width/to_str.py.

--- src/functions/dict/char_and_width/to_str.py
+++ src/functions/dict/char_and_width/to_str.py
@@ -1,30 +1,14 @@
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
-# h
-# f = lambda c, w: c*(w+2)
 f = lambda x: x['char']*(x['width']+2)
 
-def t_minus_3():
-  x = {'char': '-', 'width': 3}
-  y = '-----'
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_minus_4():
-  x = {'char': '-', 'width': 4}
-  y = '------'
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_space_4():
-  x = {'char': ' ', 'width': 4}
-  y = '      '
-  z = f(x)
-  return pxyz(x, y, z)
+t_minus_3 = lambda: pxyf({'char': '-', 'width': 3}, '-----', f)
+t_minus_4 = lambda: pxyf({'char': '-', 'width': 4}, '------', f)
+t_space_4 = lambda: pxyf({'char': ' ', 'width': 4}, '      ', f)
 
 def t():
   if not t_minus_3(): return pf('!t_minus_3')
   if not t_minus_4(): return pf('!t_minus_4')
   if not t_space_4(): return pf('!t_space_4')
-  return True
+  return 1
