30f396f5a550b5fee82b23949400e24a817fd347
John Forbes <john.robert.forbes@gmail.com>
2023-08-23 15:50:35 +1000

    Updated src/functions/dict/records_and_function/to_underlined_row.py.

--- src/functions/dict/records_and_function/to_underlined_row.py
+++ src/functions/dict/records_and_function/to_underlined_row.py
@@ -1,73 +1,36 @@
 # ignore_overlength_lines
 
-from ..records_k_branch_k_leaf.to_leaf_cell import f as h
-from ..records_k_branch_k_leaf.to_unit_cell_str import f as records_k_branch_k_leaf_to_unit_cell_str
-# from ..to_row_using_fn import f as records_to_row_using_fn
-from src.functions.dict.records_and_function.to_row_using_function import f as records_to_row_using_fn
-from hak.one.dict.rate.make import f as make_rate
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
-from src.functions.dicts.records.to_horizontal_line import f as records_to_horizontal_line
+from hak.pxyf import f as pxyf
+
+from ...dicts.records.to_horizontal_line import f as f_a
+from ..records_and_function.to_row_using_function import f as f_b
+from ..records_k_branch_k_leaf.to_leaf_cell import f as f_c
+from ..records_k_branch_k_leaf.to_unit_cell_str import f as f_d
+from data.records import records as _records
 
 # records_and_fn_to_underlined_row
-f = lambda x: [
-  records_to_row_using_fn(x),
-  records_to_horizontal_line(x['records'])
-]
+f = lambda x: [f_b(x), f_a(x['records'])]
 
-from hak.one.dict.rate.make import f as make_rate
-_records = [
-  {
-    'prices': {
-      'apples': make_rate(1, 4, {'$': 1, 'apple': -1}),
-      'bananas': make_rate(1, 2, {'$': 1, 'banana': -1})
-    },
-    'volumes': {
-      'applezzz': make_rate(1, 1, {'apple': 1}),
-      'bananazzz': make_rate(2, 1, {'banana': 1}),
-      'pearzzzzzz': make_rate(3, 1, {'pear': 1})
-    },
-    'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-  }, 
-  {
-    'prices': {
-      'apples': make_rate(3, 4, {'$': 1, 'apple': -1}),
-      'bananas': make_rate(1, 1, {'$': 1, 'banana': -1})
-    },
-    'volumes': {
-      'applezzz': make_rate(4, 1, {'apple': 1}),
-      'bananazzz': make_rate(5, 1, {'banana': 1}),
-      'pearzzzzzz': make_rate(6, 1, {'pear': 1})
-    },
-    'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-  }
-]
-
-def t_leaf_keys():
-  x = {
-    'records': _records,
-    'function': h
-  }
-  y = [
+t_leaf_keys = lambda: pxyf(
+  {'records': _records, 'function': f_c},
+  [
     '|  apples |  bananas | applezzz | bananazzz | pearzzzzzz |  zloop |',
     '|---------|----------|----------|-----------|------------|--------|'
-  ]
-  z = f(x)
-  return pxyz(x, y, z)
+  ],
+  f
+)
 
-def t_units():
-  x = {
-    'records': _records,
-    'function': records_k_branch_k_leaf_to_unit_cell_str
-  }
-  y = [
+t_units = lambda: pxyf(
+  {'records': _records, 'function': f_d},
+  [
     '| $/apple | $/banana |    apple |    banana |       pear |  zloop |',
     '|---------|----------|----------|-----------|------------|--------|'
-  ]
-  z = f(x)
-  return pxyz(x, y, z)
+  ],
+  f
+)
 
 def t():
   if not t_leaf_keys(): return pf('!t_leaf_keys')
   if not t_units(): return pf('!t_units')
-  return True
+  return 1
