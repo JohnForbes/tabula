1c3dc24a8ea54d4c99b67de2f442ca6d66d5d84e
John Forbes <john.robert.forbes@gmail.com>
2023-08-23 23:26:18 +1000

    Updated src/functions/dicts/records/to_top_border.py.

--- src/functions/dicts/records/to_top_border.py
+++ src/functions/dicts/records/to_top_border.py
@@ -1,59 +1,26 @@
 # ignore_overlength_lines
-# from .k_branch.to_branch_col_width import f as get_top_head_width
-from src.functions.dict.records_and_field_name.to_branch_col_width import f as get_top_head_width
-
-# from .fn.to_fn_applied_to_sorted_keys_of_records import f as records_and_fn_to_fn_applied_to_sorted_keys_of_records
-from src.functions.dict.records_and_function.to_sorted_keys_of_records import f as records_and_fn_to_fn_applied_to_sorted_keys_of_records
-
-# from archive.strings.to_table_row import f as cell_strings_to_table_row_string
-from src.functions.strings.cell_strings.to_table_row import f as cell_strings_to_table_row_string
-
-from hak.pxyz import f as pxyz
-from hak.pf import f as pf
 from datetime import date
+from hak.one.dict.rate.make import f as make_rate
+from hak.pf import f as pf
+from hak.pxyf import f as pxyf
+
+from data.records import records_without_date as _records
+
+from ...dict.records_and_field_name.to_branch_col_width import f as f_a
+from ...dict.records_and_function.to_sorted_keys_of_records import f as f_b
+from ...strings.cell_strings.to_table_row import f as f_c
 
 # records_to_top_border
-def f(x):
-  _a = [
-    '-'*_ for _ in records_and_fn_to_fn_applied_to_sorted_keys_of_records({
-      'records': x,
-      'function': get_top_head_width
-    })
-  ]
-  return cell_strings_to_table_row_string(_a, '-')
+f = lambda x: f_c({
+  'cell_strings': ['-'*_ for _ in f_b({'records': x, 'function': f_a})],
+  'col_separator_char': '-'
+})
 
-from hak.one.dict.rate.make import f as make_rate
-
-def t_nest():
-  x = [
-    {
-      'prices': {
-        'apples': make_rate(1, 4, {'$': 1, 'apple': -1}),
-        'bananas': make_rate(1, 2, {'$': 1, 'banana': -1})
-      },
-      'volumes': {
-        'applezzz': make_rate(1, 1, {'apple': 1}),
-        'bananazzz': make_rate(2, 1, {'banana': 1}),
-        'pearzzzzzz': make_rate(3, 1, {'pear': 1})
-      },
-      'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-    }, 
-    {
-      'prices': {
-        'apples': make_rate(3, 4, {'$': 1, 'apple': -1}),
-        'bananas': make_rate(1, 1, {'$': 1, 'banana': -1})
-      },
-      'volumes': {
-        'applezzz': make_rate(4, 1, {'apple': 1}),
-        'bananazzz': make_rate(5, 1, {'banana': 1}),
-        'pearzzzzzz': make_rate(6, 1, {'pear': 1})
-      },
-      'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-    }
-  ]
-  y = '|--------------------|-----------------------------------|--------|'
-  z = f(x)
-  return pxyz(x, y, z)
+t_nest = lambda: pxyf(
+  _records,
+  '|--------------------|-----------------------------------|--------|',
+  f
+)
 
 def t_date():
   x = [
@@ -67,10 +34,9 @@ def t_date():
     }
   ]
   y = "|------------|---------|"
-  z = f(x)
-  return pxyz(x, y, z)
+  return pxyf(x, y, f)
 
 def t():
   if not t_nest(): return pf('t_nest failed')
   # if not t_date(): return pf('t_date failed')
-  return True
+  return 1
