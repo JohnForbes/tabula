d66e23d617919acc78d67bb670ce8653bcf543c2
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 16:57:08 +1000

    Updated src/functions/dict/records_k_branch_k_leaf/to_col_as_dict.py.

--- src/functions/dict/records_k_branch_k_leaf/to_col_as_dict.py
+++ src/functions/dict/records_k_branch_k_leaf/to_col_as_dict.py
@@ -1,7 +1,8 @@
-from hak.one.dict.rate.make import f as make_rate
+from hak.one.dict.rate.make import f as rate
 from hak.pf import f as pf
 from hak.pxyf import f as pxyf
 from hak.one.dict.unit.to_str import f as unit_to_str
+from data.records import records_without_date as records_wout_date
 
 # get_column
 # records_k_branch_k_leaf_to_col_as_dict
@@ -12,42 +13,13 @@ f = lambda x: {
 }
 
 def t_prices_apples():
-  x = {
-    'records': [
-      {
-        'prices': {
-          'apples': make_rate(1, 4, {'$': 1, 'apple': -1}),
-          'bananas': make_rate(2, 4, {'$': 1, 'banana': -1})
-        },
-        'volumes': {
-          'applezzz': make_rate(1, 1, {'apple': 1}),
-          'bananazzz': make_rate(2, 1, {'banana': 1}),
-          'pearzzzzzz': make_rate(3, 1, {'pear': 1})
-        },
-        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-      },
-      {
-        'prices': {
-          'apples': make_rate(3, 4, {'$': 1, 'apple': -1}),
-          'bananas': make_rate(4, 4, {'$': 1, 'banana': -1})
-        },
-        'volumes': {
-          'applezzz': make_rate(4, 1, {'apple': 1}),
-          'bananazzz': make_rate(5, 1, {'banana': 1}),
-          'pearzzzzzz': make_rate(6, 1, {'pear': 1})
-        },
-        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-      }
-    ],
-    'k_branch': 'prices',
-    'k_leaf': 'apples'
-  }
+  x = {'records': records_wout_date, 'k_branch': 'prices', 'k_leaf': 'apples'}
   y = {
     'head': 'apples',
     'unit': '$/apple',
     'values': [
-      make_rate(1, 4, {'$': 1, 'apple': -1}),
-      make_rate(3, 4, {'$': 1, 'apple': -1})
+      rate(1, 4, {'$': 1, 'apple': -1}),
+      rate(3, 4, {'$': 1, 'apple': -1})
     ]
   }
   return pxyf(x, y, f)
