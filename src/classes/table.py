3fa789337be27e2938b8a14cb9222e8bfbeb0d05
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 15:45:52 +1000

    Updated src/classes/table.py.

--- src/classes/table.py
+++ src/classes/table.py
@@ -8,21 +8,11 @@ class Table:
   def add_record(self, record):
     self.row_count += 1
     for (k, v) in record.items():
-      self._cells[(k, self.row_count-1)] = make_cell({
-        'value': v,
-        'field_name': k
-      })
+      self._cells[(k, self.row_count-1)] = make_cell({'value': v, 'name': k})
     
   def get_column(self, column_name):
-    _cells = [
-      self._cells[(column_name, i)]
-      for i in range(self.row_count)
-    ]
-
-    return {
-      'name': column_name,
-      'cells': _cells
-    }
+    _cells = [self._cells[(column_name, i)] for i in range(self.row_count)]
+    return {'name': column_name, 'cells': _cells}
 
 f = lambda: Table()
 t = lambda: 1 # Functions externalised in functions directory
