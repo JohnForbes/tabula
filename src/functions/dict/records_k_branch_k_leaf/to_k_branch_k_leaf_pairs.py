85bf613c68d0d522dc044c634a60d3d229d1154f
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 16:03:19 +1000

    Updated src/functions/dict/records_k_branch_k_leaf/to_k_branch_k_leaf_pairs.py.

--- src/functions/dict/records_k_branch_k_leaf/to_k_branch_k_leaf_pairs.py
+++ src/functions/dict/records_k_branch_k_leaf/to_k_branch_k_leaf_pairs.py
@@ -3,16 +3,12 @@ from hak.pxyf import f as pxyf
 from hak.one.dict.rate.make import f as make_rate
 
 from ...dicts.records.to_first_record_sorted_keys import f as f_a
-from ..records_and_field_name.to_sorted_leaf_keys import f as f_b
+from ..records_and_name.to_sorted_leaf_keys import f as f_b
 
 # f_n
 # records_to_k_branch_k_leaf_pairs
 # records_to_k_b_k_l_pairs
-f = lambda x: [
-  (a, b)
-  for a in f_a(x)
-  for b in f_b({'records': x, 'field_name': a})
-]
+f = lambda x: [(a, b) for a in f_a(x) for b in f_b({'records': x, 'name': a})]
 
 t = lambda: pxyf(
   [
