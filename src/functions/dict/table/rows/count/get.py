dba9a21817637a942ce90fa1f349f61437cf5ddf
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 17:04:16 +1000

    Updated src/functions/dict/table/rows/count/get.py.

--- src/functions/dict/table/rows/count/get.py
+++ src/functions/dict/table/rows/count/get.py
@@ -1,14 +1,14 @@
 from hak.pxyf import f as pxyf
 
 from ...insert_records import f as insert_records
-from ...make import f as make_table
+from ...make import f as table
 
 # row_count
 f = lambda x: len(x['row_order'])
 
 t = lambda: pxyf(
   insert_records({
-    'table': make_table(),
+    'table': table(),
     'records': [
       {'a': 0, 'b':  1, 'c':  2},
       {'a': 3, 'b':  4, 'c':  5},
