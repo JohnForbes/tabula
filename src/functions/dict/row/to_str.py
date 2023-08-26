7f637cd6c7b00ceff9cacfa2f905826f09540fcb
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 17:00:58 +1000

    Updated src/functions/dict/row/to_str.py.

--- src/functions/dict/row/to_str.py
+++ src/functions/dict/row/to_str.py
@@ -1,16 +1,16 @@
 from hak.pf import f as pf
 from hak.pxyf import f as pxyf
 
-from src.functions.dict.cell.make import f as make_cell
+from src.functions.dict.cell.make import f as cell
 from src.functions.dict.cell.to_str import f as cell_to_str
 
 f = lambda x: '| '+' | '.join([cell_to_str(x[k]) for k in x])+' |'
 
 def t_a():
   x = {
-    'a': make_cell({'value': 1, 'name': 'a'}),
-    'b': make_cell({'value': 2, 'name': 'b'}),
-    'c': make_cell({'value': 3, 'name': 'c'})
+    'a': cell({'value': 1, 'name': 'a'}),
+    'b': cell({'value': 2, 'name': 'b'}),
+    'c': cell({'value': 3, 'name': 'c'})
   }
   y = '| 1 | 2 | 3 |'
   return pxyf(x, y, f)
