9e0dba5665ac6cef3e130f3dc98177c0392a3b26
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 15:42:54 +1000

    Updated scrap.py.

--- scrap.py
+++ scrap.py
@@ -1,25 +1,4 @@
-from src.functions.dict.cell.make import f as make_cell
-
-class Table:
-  def __init__(self):
-    self._cells = {}
-    self.row_count = 0
-  
-  def add_record(self, record):
-    self.row_count += 1
-    for (k, v) in record.items():
-      self._cells[(k, self.row_count-1)] = make_cell({'value': v, 'field_name': k})
-    
-  def get_column(self, column_name):
-    _cells = [
-      self._cells[(column_name, i)]
-      for i in range(self.row_count)
-    ]
-
-    return {
-      'name': column_name,
-      'cells': _cells
-    }
+from src.classes.table import Table
 
 t = Table()
 t.add_record({'a': 1})
