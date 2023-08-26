c6013ad13cca0a111484ebf9b7eb3d0a6c658b92
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 17:03:21 +1000

    Updated src/functions/dict/table/columns/order/get.py.

--- src/functions/dict/table/columns/order/get.py
+++ src/functions/dict/table/columns/order/get.py
@@ -1,13 +1,13 @@
 from hak.pxyf import f as pxyf
 
 from ...insert_record import f as insert_record
-from ...make import f as make_table
+from ...make import f as table
 
 # column_order
 f = lambda x: x['column_order']
 
 t = lambda: pxyf(
-  insert_record({'table': make_table(), 'record': {'a': 0, 'b': 1, 'c': 2}}),
+  insert_record({'table': table(), 'record': {'a': 0, 'b': 1, 'c': 2}}),
   ['a', 'b', 'c'],
   f
 )
