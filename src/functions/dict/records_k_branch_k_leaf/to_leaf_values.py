91f7bada34ee2e5ac1991a302409b312340f9285
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 16:59:11 +1000

    Updated src/functions/dict/records_k_branch_k_leaf/to_leaf_values.py.

--- src/functions/dict/records_k_branch_k_leaf/to_leaf_values.py
+++ src/functions/dict/records_k_branch_k_leaf/to_leaf_values.py
@@ -1,212 +1,40 @@
 from hak.one.dict.rate.to_str import f as rate_to_str
 from hak.pf import f as pf
 from hak.pxyf import f as pxyf
-from hak.one.dict.rate.make import f as make_rate
+from hak.one.dict.rate.make import f as rate
 
 # records_k_branch_k_leaf_to_leaf_values
 f = lambda x: [rate_to_str(r[x['k_branch']][x['k_leaf']]) for r in x['records']]
 
+from data.records import records_without_date as _records
+
 def t_prices_apples():
-  x = {
-    'records': [
-      {
-        'prices': {
-          'apples': make_rate(1, 4, {'$': 1, 'apple': -1}),
-          'bananas': make_rate(1, 2, {'$': 1, 'banana': -1})
-        },
-        'volumes': {
-          'applezzz': make_rate(1, 1, {'apple': 1}),
-          'bananazzz': make_rate(2, 1, {'banana': 1}),
-          'pearzzzzzz': make_rate(3, 1, {'pear': 1})
-        },
-        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-      }, 
-      {
-        'prices': {
-          'apples': make_rate(3, 4, {'$': 1, 'apple': -1}),
-          'bananas': make_rate(1, 1, {'$': 1, 'banana': -1})
-        },
-        'volumes': {
-          'applezzz': make_rate(4, 1, {'apple': 1}),
-          'bananazzz': make_rate(5, 1, {'banana': 1}),
-          'pearzzzzzz': make_rate(6, 1, {'pear': 1})
-        },
-        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-      }
-    ],
-    'k_branch': 'prices',
-    'k_leaf':   'apples'
-  }
+  x = {'records': _records, 'k_branch': 'prices', 'k_leaf': 'apples'}
   y = ['0.25', '0.75']
   return pxyf(x, y, f)
 
 def t_prices_bananas():
-  x = {
-    'records': [
-      {
-        'prices': {
-          'apples': make_rate(1, 4, {'$': 1, 'apple': -1}),
-          'bananas': make_rate(1, 2, {'$': 1, 'banana': -1})
-        },
-        'volumes': {
-          'applezzz': make_rate(1, 1, {'apple': 1}),
-          'bananazzz': make_rate(2, 1, {'banana': 1}),
-          'pearzzzzzz': make_rate(3, 1, {'pear': 1})
-        },
-        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-      }, 
-      {
-        'prices': {
-          'apples': make_rate(3, 4, {'$': 1, 'apple': -1}),
-          'bananas': make_rate(1, 1, {'$': 1, 'banana': -1})
-        },
-        'volumes': {
-          'applezzz': make_rate(4, 1, {'apple': 1}),
-          'bananazzz': make_rate(5, 1, {'banana': 1}),
-          'pearzzzzzz': make_rate(6, 1, {'pear': 1})
-        },
-        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-      }
-    ],
-    'k_branch': 'prices',
-    'k_leaf':   'bananas'
-  }
+  x = {'records': _records, 'k_branch': 'prices', 'k_leaf': 'bananas'}
   y = ['0.50', '1.00']
   return pxyf(x, y, f)
 
 def t_volumes_applezzz():
-  x = {
-    'records': [
-      {
-        'prices': {
-          'apples': make_rate(1, 4, {'$': 1, 'apple': -1}),
-          'bananas': make_rate(1, 2, {'$': 1, 'banana': -1})
-        },
-        'volumes': {
-          'applezzz': make_rate(1, 1, {'apple': 1}),
-          'bananazzz': make_rate(2, 1, {'banana': 1}),
-          'pearzzzzzz': make_rate(3, 1, {'pear': 1})
-        },
-        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-      }, 
-      {
-        'prices': {
-          'apples': make_rate(3, 4, {'$': 1, 'apple': -1}),
-          'bananas': make_rate(1, 1, {'$': 1, 'banana': -1})
-        },
-        'volumes': {
-          'applezzz': make_rate(4, 1, {'apple': 1}),
-          'bananazzz': make_rate(5, 1, {'banana': 1}),
-          'pearzzzzzz': make_rate(6, 1, {'pear': 1})
-        },
-        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-      }
-    ],
-    'k_branch': 'volumes',
-    'k_leaf':   'applezzz'
-  }
+  x = {'records': _records, 'k_branch': 'volumes', 'k_leaf': 'applezzz'}
   y = ['1.00', '4.00']
   return pxyf(x, y, f)
 
 def t_volumes_bananazzz():
-  x = {
-    'records': [
-      {
-        'prices': {
-          'apples': make_rate(1, 4, {'$': 1, 'apple': -1}),
-          'bananas': make_rate(1, 2, {'$': 1, 'banana': -1})
-        },
-        'volumes': {
-          'applezzz': make_rate(1, 1, {'apple': 1}),
-          'bananazzz': make_rate(2, 1, {'banana': 1}),
-          'pearzzzzzz': make_rate(3, 1, {'pear': 1})
-        },
-        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-      }, 
-      {
-        'prices': {
-          'apples': make_rate(3, 4, {'$': 1, 'apple': -1}),
-          'bananas': make_rate(1, 1, {'$': 1, 'banana': -1})
-        },
-        'volumes': {
-          'applezzz': make_rate(4, 1, {'apple': 1}),
-          'bananazzz': make_rate(5, 1, {'banana': 1}),
-          'pearzzzzzz': make_rate(6, 1, {'pear': 1})
-        },
-        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-      }
-    ],
-    'k_branch': 'volumes',
-    'k_leaf':   'bananazzz'
-  }
+  x = {'records': _records, 'k_branch': 'volumes', 'k_leaf': 'bananazzz'}
   y = ['2.00', '5.00']
   return pxyf(x, y, f)
 
 def t_volumes_pearzzzzzz():
-  x = {
-    'records': [
-      {
-        'prices': {
-          'apples': make_rate(1, 4, {'$': 1, 'apple': -1}),
-          'bananas': make_rate(1, 2, {'$': 1, 'banana': -1})
-        },
-        'volumes': {
-          'applezzz': make_rate(1, 1, {'apple': 1}),
-          'bananazzz': make_rate(2, 1, {'banana': 1}),
-          'pearzzzzzz': make_rate(3, 1, {'pear': 1})
-        },
-        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-      }, 
-      {
-        'prices': {
-          'apples': make_rate(3, 4, {'$': 1, 'apple': -1}),
-          'bananas': make_rate(1, 1, {'$': 1, 'banana': -1})
-        },
-        'volumes': {
-          'applezzz': make_rate(4, 1, {'apple': 1}),
-          'bananazzz': make_rate(5, 1, {'banana': 1}),
-          'pearzzzzzz': make_rate(6, 1, {'pear': 1})
-        },
-        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-      }
-    ],
-    'k_branch': 'volumes',
-    'k_leaf':   'pearzzzzzz'
-  }
+  x = {'records': _records, 'k_branch': 'volumes', 'k_leaf': 'pearzzzzzz'}
   y = ['3.00', '6.00']
   return pxyf(x, y, f)
 
 def t_zloops_zloop():
-  x = {
-    'records': [
-      {
-        'prices': {
-          'apples': make_rate(1, 4, {'$': 1, 'apple': -1}),
-          'bananas': make_rate(1, 2, {'$': 1, 'banana': -1})
-        },
-        'volumes': {
-          'applezzz': make_rate(1, 1, {'apple': 1}),
-          'bananazzz': make_rate(2, 1, {'banana': 1}),
-          'pearzzzzzz': make_rate(3, 1, {'pear': 1})
-        },
-        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-      }, 
-      {
-        'prices': {
-          'apples': make_rate(3, 4, {'$': 1, 'apple': -1}),
-          'bananas': make_rate(1, 1, {'$': 1, 'banana': -1})
-        },
-        'volumes': {
-          'applezzz': make_rate(4, 1, {'apple': 1}),
-          'bananazzz': make_rate(5, 1, {'banana': 1}),
-          'pearzzzzzz': make_rate(6, 1, {'pear': 1})
-        },
-        'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-      }
-    ],
-    'k_branch': 'zloops',
-    'k_leaf':   'zloop'
-  }
+  x = {'records': _records, 'k_branch': 'zloops', 'k_leaf': 'zloop'}
   y = ['7.00', '7.00']
   return pxyf(x, y, f)
 
