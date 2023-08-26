3a8142893b1b7224df9b582629006443c497cc52
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 15:57:21 +1000

    Updated src/functions/dict/field_name_and_values/width/get.py.

--- src/functions/dict/field_name_and_values/width/get.py
+++ src/functions/dict/field_name_and_values/width/get.py
@@ -2,22 +2,12 @@ from hak.pf import f as pf
 from hak.pxyf import f as pxyf
 
 # get_width
-# f = lambda k, values: max(len(k), *[len(str(v)) for v in values])
-f = lambda x: max(len(x['field_name']), *[len(str(v)) for v in x['values']])
+f = lambda x: max(len(x['name']), *[len(str(v)) for v in x['values']])
 
-t_field_name_dominant = lambda: pxyf(
-  {'field_name': 'foo', 'values': [0, 1, 2, 3]},
-  3,
-  f
-)
-
-t_value_dominant = lambda: pxyf(
-  {'field_name': 'foo', 'values': [0, 1000, 2000, 3000]},
-  4,
-  f
-)
+t_name_dominant = lambda: pxyf({'name': 'foo', 'values': [0, 1, 2, 3]}, 3, f)
+t_value_dominant = lambda: pxyf({'name': 'foo', 'values': [0, 1000]}, 4, f)
 
 def t():
-  if not t_field_name_dominant(): return pf('!t_field_name_dominant')
+  if not t_name_dominant(): return pf('!t_name_dominant')
   if not t_value_dominant(): return pf('!t_value_dominant')
   return 1
