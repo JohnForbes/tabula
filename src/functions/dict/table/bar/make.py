0fdcce06d8163822cdfc9216d88916b5260f6fb0
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 17:02:18 +1000

    Updated src/functions/dict/table/bar/make.py.

--- src/functions/dict/table/bar/make.py
+++ src/functions/dict/table/bar/make.py
@@ -2,30 +2,21 @@ from hak.pf import f as pf
 from hak.pxyf import f as pxyf
 
 from src.functions.dict.table.insert_records import f as insert_records
-from src.functions.dict.table.make import f as make_table
+from src.functions.dict.table.make import f as table
 
-f = lambda table: (
-  '|-'+'-|-'.join(['-'*len(f'{c}') for c in table['column_order']])+'-|'
-)
-
-# make_bar
-# f = lambda x: "|-"+'-|-'.join(['-'*x['widths'][k] for k in x['names']])+"-|"
+f = lambda x: '|-'+'-|-'.join(['-'*len(f'{c}') for c in x['column_order']])+'-|'
 
 def t_ac():
   x = insert_records({
-    'table': make_table(),
-    'records': [
-      {'a': 0, 'c': 2},
-      {'a': 3, 'c': 5},
-      {'a': 6, 'c': 8}
-    ]
+    'table': table(),
+    'records': [{'a': 0, 'c': 2}, {'a': 3, 'c': 5}, {'a': 6, 'c': 8}]
   })
   y = '|---|---|'
   return pxyf(x, y, f, new_line=1)
 
 def t_abc():
   x = insert_records({
-    'table': make_table(),
+    'table': table(),
     'records': [
       {'a': 0, 'b': 1, 'c': 2},
       {'a': 3, 'b': 4, 'c': 5},
