3580ab486c21674be6e7d19946e87ef2c9f04cea
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 20:38:40 +1000

    Updated src/functions/dicts/records/to_value_rows.py.

--- src/functions/dicts/records/to_value_rows.py
+++ src/functions/dicts/records/to_value_rows.py
@@ -1,23 +1,12 @@
 from hak.pxyf import f as pxyf
 
-from ...dict.records_and_index.to_padded_cell_values_of_col_width import f as fn
 from data.records import records_without_date as _records
 
+from ...dict.records_and_index.to_padded_cell_values_of_col_width import f as g
+
 f = lambda x: [
-  '| '+' | '.join(fn({'records': x, 'index': r_i}))+' |'
-  for r_i
-  in range(len(x))
+  '| '+' | '.join(g({'records': x, 'index': i}))+' |' for i in range(len(x))
 ]
-
-# def f(x):
-  # print(f'x: {x}')
-  # print()
-  # result = []
-  # for r_i in range(len(x)):
-  #   _ = fn({'records': x, 'index': r_i})
-  #   result.append('| '+' | '.join(_)+' |')
-
-  # return result
 
 t = lambda: pxyf(
   _records,
