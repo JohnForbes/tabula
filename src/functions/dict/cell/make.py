d42774325b4a03d32cc4fd87722105573ebc9038
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 15:53:25 +1000

    Updated src/functions/dict/cell/make.py.

--- src/functions/dict/cell/make.py
+++ src/functions/dict/cell/make.py
@@ -5,18 +5,18 @@
 # from hak.one.dict.rate.to_num import f as to_num
 # from hak.one.string.colour.decolour import f as decol
 from datetime import date
+from hak.many.dicts.a_into_b import f as a_into_b
 from hak.one.dict.rate.make import f as make_rate
 from hak.one.get_datatype import f as detect_type
 from hak.pf import f as pf
 from hak.pxyf import f as pxyf
+from hak.pxyz import f as pxyz
+from hak.one.number.int.random.make import f as make_random_integer
+from hak.one.string.random.make import f as make_random_string
 
 # make_cell
 # src.cell.make
-f = lambda x: {
-  'value': x['value'],
-  'datatype': detect_type(x['value']),
-  'field_name': x['field_name'],
-}
+f = lambda x: a_into_b({'datatype': detect_type(x['value'])}, x)
 
 # def f(x):
 #   _width = x['width']
@@ -43,44 +43,44 @@ f = lambda x: {
 #   return left_pad + f'{_val_str:>{_width}}'
 
 def t_0():
-  x = {'value': 0, 'field_name': 'i'}
-  y = {'value': 0, 'field_name': 'i', 'datatype': 'int'}
+  x = {'value': 0, 'name': 'i'}
+  y = {'value': 0, 'name': 'i', 'datatype': 'int'}
   return pxyf(x, y, f)
 
 def t_a():
   # cell_dict
-  x = {'value': 'a', 'field_name': 'A'}
-  y = {'value': 'a', 'field_name': 'A', 'datatype': 'str'}
+  x = {'value': 'a', 'name': 'A'}
+  y = {'value': 'a', 'name': 'A', 'datatype': 'str'}
   return pxyf(x, y, f)
 
 def t_rate():
-  x = {'value': make_rate(1, 1, {'m': 1}), 'field_name': 'distance'}
+  x = {'value': make_rate(1, 1, {'m': 1}), 'name': 'distance'}
   y = {
     'value': {'numerator': 1, 'denominator': 1, 'unit': {'m': 1}},
-    'field_name': 'distance',
+    'name': 'distance',
     'datatype': 'rate'
   }
   return pxyf(x, y, f)
 
 def t_date():
-  x = {'value': date(2022, 4, 5), 'field_name': 'date'}
-  y = {'value': date(2022, 4, 5), 'field_name': 'date', 'datatype': 'date'}
+  x = {'value': date(2022, 4, 5), 'name': 'date'}
+  y = {'value': date(2022, 4, 5), 'name': 'date', 'datatype': 'date'}
   return pxyf(x, y, f)
 
 def t_description():
-  x = {'value': 'Purchased USD with AUD', 'field_name': 'description'}
+  x = {'value': 'Purchased USD with AUD', 'name': 'description'}
   y = {
     'value': 'Purchased USD with AUD',
-    'field_name': 'description',
+    'name': 'description',
     'datatype': 'str'
   }
   return pxyf(x, y, f)
 
 def t_USD_rate():
-  x = {'value': make_rate(5472, 1, {'USD': 1}), 'field_name': 'USD Rate'}
+  x = {'value': make_rate(5472, 1, {'USD': 1}), 'name': 'USD Rate'}
   y = {
     'value': {'numerator': 5472, 'denominator': 1, 'unit': {'USD': 1}},
-    'field_name': 'USD Rate',
+    'name': 'USD Rate',
     'datatype': 'rate'
   }
   return pxyf(x, y, f)
@@ -88,28 +88,38 @@ def t_USD_rate():
 def t_A_nabtrade_cash_AUD():
   x = {
     'value': make_rate(-7350.89, 1, {'AUD': 1}),
-    'field_name': 'nabtrade_cash_AUD'
+    'name': 'nabtrade_cash_AUD'
   }
   y = {
     'value': {'numerator': -735089, 'denominator': 100, 'unit': {'AUD': 1}},
-    'field_name': 'nabtrade_cash_AUD',
+    'name': 'nabtrade_cash_AUD',
     'datatype': 'rate'
   }
   return pxyf(x, y, f)
 
 def t_rate_0():
-  x = {'value': make_rate(0, 1, {'m': 1}), 'field_name': 'description'}
+  x = {'value': make_rate(0, 1, {'m': 1}), 'name': 'description'}
   y = {
     'value': {'numerator': 0, 'denominator': 1, 'unit': {'m': 1}},
-    'field_name': 'description',
+    'name': 'description',
     'datatype': 'rate'
   }
   return pxyf(x, y, f)
 
 def t_none():
-  x = {'value': None, 'field_name': 'foo'}
-  y = {'value': None, 'field_name': 'foo', 'datatype': 'none'}
+  x = {'value': None, 'name': 'foo'}
+  y = {'value': None, 'name': 'foo', 'datatype': 'none'}
   return pxyf(x, y, f)
+
+def t_drift():
+  _value = make_random_integer(-9, 9)
+  _name = make_random_string()
+  _x = {'value': _value, 'name': _name}
+  x = {'value': _value, 'name': _name}
+  y = {'value': _value, 'name': _name, 'datatype': 'int'}
+  z = f(x)
+  if _x != x: return pf(f'drift of original dict occurred; _x: {_x}; x: {x}')
+  return pxyz(x, y, z)
 
 def t():
   if not t_0(): return pf('!t_0')
@@ -121,4 +131,5 @@ def t():
   if not t_rate(): return pf('!t_rate')
   if not t_USD_rate(): return pf('!t_USD_rate')
   if not t_none(): return pf('!t_none')
+  if not t_drift(): return pf('!t_drift')
   return 1
