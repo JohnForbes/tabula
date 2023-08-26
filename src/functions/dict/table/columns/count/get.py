66229413533be6a0826337ee78285ed5292a3b81
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 17:03:11 +1000

    Updated src/functions/dict/table/columns/count/get.py.

--- src/functions/dict/table/columns/count/get.py
+++ src/functions/dict/table/columns/count/get.py
@@ -1,13 +1,11 @@
 from hak.pxyf import f as pxyf
 
-from src.functions.dict.table.insert_record import f as insert_record
-from src.functions.dict.table.make import f as make_table
+from ...insert_record import f as insert_record
+from ...make import f as table
 
 # column_count
 f = lambda x: len(x['column_order'])
 
 t = lambda: pxyf(
-  insert_record({'table': make_table(), 'record': {'a': 0, 'b': 1, 'c': 2}}),
-  3,
-  f
+  insert_record({'table': table(), 'record': {'a': 0, 'b': 1, 'c': 2}}), 3, f
 )
