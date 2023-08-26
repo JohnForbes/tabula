d6ec8f15a3f09e762c0e6d1c0da8936c7115a5fc
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 16:57:41 +1000

    Updated src/functions/dict/records_k_branch_k_leaf/to_k_branch_k_leaf_pairs.py.

--- src/functions/dict/records_k_branch_k_leaf/to_k_branch_k_leaf_pairs.py
+++ src/functions/dict/records_k_branch_k_leaf/to_k_branch_k_leaf_pairs.py
@@ -1,9 +1,11 @@
 # ignore_overlength_lines
 from hak.pxyf import f as pxyf
-from hak.one.dict.rate.make import f as make_rate
+from hak.one.dict.rate.make import f as rate
 
 from ...dicts.records.to_first_record_sorted_keys import f as f_a
 from ..records_and_name.to_sorted_leaf_keys import f as f_b
+
+from data.records import records_without_date as records_wout_date
 
 # f_n
 # records_to_k_branch_k_leaf_pairs
@@ -11,32 +13,7 @@ from ..records_and_name.to_sorted_leaf_keys import f as f_b
 f = lambda x: [(a, b) for a in f_a(x) for b in f_b({'records': x, 'name': a})]
 
 t = lambda: pxyf(
-  [
-    {
-      'prices': {
-        'apples': make_rate(1, 4, {'$': 1, 'apple': -1}),
-        'bananas': make_rate(2, 4, {'$': 1, 'banana': -1})
-      },
-      'volumes': {
-        'applezzz': make_rate(1, 1, {'apple': 1}),
-        'bananazzz': make_rate(2, 1, {'banana': 1}),
-        'pearzzzzzz': make_rate(3, 1, {'pear': 1})
-      },
-      'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-    },
-    {
-      'prices': {
-        'apples': make_rate(3, 4, {'$': 1, 'apple': -1}),
-        'bananas': make_rate(4, 4, {'$': 1, 'banana': -1})
-      },
-      'volumes': {
-        'applezzz': make_rate(4, 1, {'apple': 1}),
-        'bananazzz': make_rate(5, 1, {'banana': 1}),
-        'pearzzzzzz': make_rate(6, 1, {'pear': 1})
-      },
-      'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
-    }
-  ],
+  records_wout_date,
   [
     ('prices',  'apples'    ),
     ('prices',  'bananas'   ),
