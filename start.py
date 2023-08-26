b8469024f5fe7e59787d17732d9bb494207a966d
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 15:44:33 +1000

    Updated start.py.

--- start.py
+++ start.py
@@ -1,3 +1,21 @@
-from src.functions.main import f as main
-x = []
-if __name__ == '__main__': main(x)
+from src.classes.table import Table
+from src.classes.record import Record
+from hak.one.dict.rate.make import f as rate
+from datetime import date
+
+table = Table()
+record = Record({
+  'integer': 10,
+  'string': 'abc',
+  'price': rate(1, 2, {'$': 1, 'item': -1}),
+  'date': date.today()
+})
+
+print(record)
+
+table.add_record(record)
+
+print(table.column_names)
+
+print(table.header)
+print(table.columns['integer'])
