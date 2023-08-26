5cf443af8a7ff66673e55b5c5f551cfea897e433
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 17:04:02 +1000

    Updated src/functions/dict/table/insert_records.py.

--- src/functions/dict/table/insert_records.py
+++ src/functions/dict/table/insert_records.py
@@ -1,8 +1,8 @@
 from hak.pxyf import f as pxyf
 
-from ..cell.make import f as make_cell
+from ..cell.make import f as cell
 from .insert_record import f as insert_record
-from .make import f as make_table
+from .make import f as table
 
 f = lambda x: (
   f({
@@ -15,7 +15,7 @@ f = lambda x: (
 
 def t():
   x = {
-    'table': make_table(),
+    'table': table(),
     'records': [
       {'a': 0, 'b': 1, 'c': 2},
       {'a': 3, 'b': 4, 'c': 5},
@@ -26,15 +26,15 @@ def t():
     'column_order': ['a', 'b', 'c'],
     'row_order': [0, 1, 2],
     'cells': {
-      ('a', 0): make_cell({'value': 0, 'name': 'a'}),
-      ('b', 0): make_cell({'value': 1, 'name': 'b'}),
-      ('c', 0): make_cell({'value': 2, 'name': 'c'}),
-      ('a', 1): make_cell({'value': 3, 'name': 'a'}),
-      ('b', 1): make_cell({'value': 4, 'name': 'b'}),
-      ('c', 1): make_cell({'value': 5, 'name': 'c'}),
-      ('a', 2): make_cell({'value': 6, 'name': 'a'}),
-      ('b', 2): make_cell({'value': 7, 'name': 'b'}),
-      ('c', 2): make_cell({'value': 8, 'name': 'c'}),
+      ('a', 0): cell({'value': 0, 'name': 'a'}),
+      ('b', 0): cell({'value': 1, 'name': 'b'}),
+      ('c', 0): cell({'value': 2, 'name': 'c'}),
+      ('a', 1): cell({'value': 3, 'name': 'a'}),
+      ('b', 1): cell({'value': 4, 'name': 'b'}),
+      ('c', 1): cell({'value': 5, 'name': 'c'}),
+      ('a', 2): cell({'value': 6, 'name': 'a'}),
+      ('b', 2): cell({'value': 7, 'name': 'b'}),
+      ('c', 2): cell({'value': 8, 'name': 'c'}),
     }
   }
   return pxyf(x, y, f, new_line=1)
