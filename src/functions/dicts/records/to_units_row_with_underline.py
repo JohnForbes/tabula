72e3f507eb53f7fe0c5dc2a67f9720e1c0832b3b
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 17:15:09 +1000

    Updated src/functions/dicts/records/to_units_row_with_underline.py.

--- src/functions/dicts/records/to_units_row_with_underline.py
+++ src/functions/dicts/records/to_units_row_with_underline.py
@@ -1,10 +1,10 @@
 # ignore_overlength_lines
-from hak.one.dict.rate.make import f as make_rate
+from hak.one.dict.rate.make import f as rate
 from hak.pf import f as pf
 from hak.pxyf import f as pxyf
 
-from src.functions.dict.records_and_function.to_underlined_row import f as f_a
-from src.functions.dict.records_k_branch_k_leaf.to_unit_cell_str import f as f_b
+from ...dict.records_and_function.to_underlined_row import f as f_a
+from ...dict.records_k_branch_k_leaf.to_unit_cell_str import f as f_b
 
 # f_units
 # records_to_units_row_with_underline
@@ -14,27 +14,27 @@ def t_a():
   x = [
     {
       'prices': {
-        'apples': make_rate(1, 4, {'$': 1, 'apple': -1}),
-        'bananas': make_rate(1, 2, {'$': 1, 'banana': -1})
+        'apples': rate(1, 4, {'$': 1, 'apple': -1}),
+        'bananas': rate(1, 2, {'$': 1, 'banana': -1})
       },
       'volumes': {
-        'applezzz': make_rate(1, 1, {'apple': 1}),
-        'bananazzz': make_rate(2, 1, {'banana': 1}),
-        'pearzzzzzz': make_rate(3, 1, {'pear': 1})
+        'applezzz': rate(1, 1, {'apple': 1}),
+        'bananazzz': rate(2, 1, {'banana': 1}),
+        'pearzzzzzz': rate(3, 1, {'pear': 1})
       },
-      'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
+      'zloops': {'zloop': rate(7, 1, {'zloop': 1})}
     },
     {
       'prices': {
-        'apples': make_rate(3, 4, {'$': 1, 'apple': -1}),
-        'bananas': make_rate(1, 1, {'$': 1, 'banana': -1})
+        'apples': rate(3, 4, {'$': 1, 'apple': -1}),
+        'bananas': rate(1, 1, {'$': 1, 'banana': -1})
       },
       'volumes': {
-        'applezzz': make_rate(4, 1, {'apple': 1}),
-        'bananazzz': make_rate(5, 1, {'banana': 1}),
-        'pearzzzzzz': make_rate(6, 1, {'pear': 1})
+        'applezzz': rate(4, 1, {'apple': 1}),
+        'bananazzz': rate(5, 1, {'banana': 1}),
+        'pearzzzzzz': rate(6, 1, {'pear': 1})
       },
-      'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
+      'zloops': {'zloop': rate(7, 1, {'zloop': 1})}
     }
   ]
   y = [
@@ -47,27 +47,27 @@ def t_b():
   x = [
     {
       'prices': {
-        'apples': make_rate(1, 4, {'$': 1, 'apple': -1}),
-        'bananas': make_rate(2, 4, {'$': 1, 'banana': -1})
+        'apples': rate(1, 4, {'$': 1, 'apple': -1}),
+        'bananas': rate(2, 4, {'$': 1, 'banana': -1})
       },
       'volumes': {
-        'applezzz': make_rate(1, 1, {'apple': 1}),
-        'bananazzz': make_rate(2, 1, {'banana': 1}),
-        'pearzzzzzz': make_rate(3, 1, {'pear': 1})
+        'applezzz': rate(1, 1, {'apple': 1}),
+        'bananazzz': rate(2, 1, {'banana': 1}),
+        'pearzzzzzz': rate(3, 1, {'pear': 1})
       },
-      'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
+      'zloops': {'zloop': rate(7, 1, {'zloop': 1})}
     },
     {
       'prices': {
-        'apples': make_rate(3, 4, {'$': 1, 'apple': -1}),
-        'bananas': make_rate(4, 4, {'$': 1, 'banana': -1})
+        'apples': rate(3, 4, {'$': 1, 'apple': -1}),
+        'bananas': rate(4, 4, {'$': 1, 'banana': -1})
       },
       'volumes': {
-        'applezzz': make_rate(4, 1, {'apple': 1}),
-        'bananazzz': make_rate(5, 1, {'banana': 1}),
-        'pearzzzzzz': make_rate(6, 1, {'pear': 1})
+        'applezzz': rate(4, 1, {'apple': 1}),
+        'bananazzz': rate(5, 1, {'banana': 1}),
+        'pearzzzzzz': rate(6, 1, {'pear': 1})
       },
-      'zloops': {'zloop': make_rate(7, 1, {'zloop': 1})}
+      'zloops': {'zloop': rate(7, 1, {'zloop': 1})}
     }
   ]
   y = [
