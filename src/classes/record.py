92606315f7fdc7805c55ca042bbc459f8a85f42c
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 15:43:52 +1000

    Updated src/classes/record.py.

--- src/classes/record.py
+++ src/classes/record.py
@@ -1,5 +1,11 @@
+from hak.one.dict.is_a import f as is_dict
+
 class Record:
-  def __init__(self): pass
+  def __init__(self, x):
+    if not is_dict(x): raise TypeError(f'not dict; x: {x}; type(x): {type(x)}')
+    self.value = x
+  
+  __str__ = lambda self: str(self.value)
 
 f = lambda: Record()
 t = lambda: 1 # Functions externalised in functions directory
