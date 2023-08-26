96581d371fb736020ab89ea8c9526e0bb35ef654
John Forbes <john.robert.forbes@gmail.com>
2023-08-26 17:06:31 +1000

    Updated src/functions/dict/table/to_rows_str.py.

--- src/functions/dict/table/to_rows_str.py
+++ src/functions/dict/table/to_rows_str.py
@@ -1,23 +1,25 @@
 from hak.pf import f as pf
 from hak.pxyf import f as pxyf
 
-from src.functions.dict.row.to_str import f as row_to_str
-from src.functions.dict.table_and_row_name.to_row import f as row_name_to_row
-from src.functions.dict.table.insert_records import f as insert_records
-from src.functions.dict.table.make import f as make_table
+from ..row.to_str import f as row_to_str
+from ..table_and_row_name.to_row import f as row_name_to_row
+from .insert_records import f as insert_records
+from .make import f as table
 
-def f(x):
-  rows = [row_name_to_row({'table': x, 'name': n}) for n in x['row_order']]
-  return '\n'.join([row_to_str(r) for r in rows])
+f = lambda x: '\n'.join([
+  row_to_str(r)
+  for r
+  in [
+    row_name_to_row({'table': x, 'name': n})
+    for n
+    in x['row_order']
+  ]
+])
 
 def t_ab():
   x = insert_records({
-    'table': make_table(),
-    'records': [
-      {'a': 0, 'b': 1},
-      {'a': 3, 'b': 4},
-      {'a': 6, 'b': 7}
-    ]
+    'table': table(),
+    'records': [{'a': 0, 'b': 1}, {'a': 3, 'b': 4}, {'a': 6, 'b': 7}]
   })
   y = '\n'.join([
     '|   | 1 |',
@@ -28,12 +30,8 @@ def t_ab():
 
 def t_ac():
   x = insert_records({
-    'table': make_table(),
-    'records': [
-      {'a': 0, 'c': 2},
-      {'a': 3, 'c': 5},
-      {'a': 6, 'c': 8}
-    ]
+    'table': table(),
+    'records': [{'a': 0, 'c': 2}, {'a': 3, 'c': 5}, {'a': 6, 'c': 8}]
   })
   y = '\n'.join([
     '|   | 2 |',
@@ -44,7 +42,7 @@ def t_ac():
 
 def t_abc():
   x = insert_records({
-    'table': make_table(),
+    'table': table(),
     'records': [
       {'a': 0, 'b': 1, 'c': 2},
       {'a': 3, 'b': 4, 'c': 5},
