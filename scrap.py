6a24d707a529b7e801873b252d0f0c17c6ba492d
John Forbes <john.robert.forbes@gmail.com>
2023-08-28 20:25:05 +1000

    Updated scrap.py.

--- scrap.py
+++ scrap.py
@@ -1,11 +1,44 @@
+from src.classes.cell import Cell
+from hak.one.dict.rate.make import f as rate
 from src.classes.table import Table
+from src.classes.column import Column
 
-t = Table()
-t.add_record({'a': 1})
-t.add_record({'a': 2, 'b': 'boo'})
+# cell = Cell(value=rate(710, 113, {'a': 1}))
+# print([str(cell)])
+# print(cell.width)
 
-print(t._cells)
-print(t.row_count)
+# cell = Cell(value=True)
+# print(str(cell))
+# print(cell.width)
 
-column_a = t.get_column('a')
-print(column_a)
+# print(isinstance(cell, Cell))
+
+table = Table()
+_a = 'aaa'
+table.add_record({_a: 1, 'b': 2, 'c': 3})
+table.add_record({_a: 4, 'b': 5, 'c': 6})
+table.add_record({_a: 7, 'b': 8, 'c': 9})
+
+# for c in table._cells:
+#   print(c, table._cells[c])
+
+# column = table.get_column(_a)
+# print(column)
+
+column = Column(_a, table)
+# print(column.width)
+# for c in column.cells:
+#   print(c)
+
+# print(column.width)
+
+
+table.add_record({_a: 10, 'b': 11, 'c': 12})
+# print()
+# column = Column(_a, table)
+# print(column.width)
+# for c in column.cells:
+#   print(c)
+
+print(Column(_a, table))
+print(Column('b', table))
