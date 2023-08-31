a0b4952dae138e0af5ce3b7d8bcf4e0e5a990077
John Forbes <john.robert.forbes@gmail.com>
2023-08-31 23:31:55 +1000

    Updated scrap.py.

--- scrap.py
+++ scrap.py
@@ -7,7 +7,7 @@ records = [
     'date': date(2023, 1, 1),
     'cecil': {
       'robert': {'john': {'zenn': 0, 'rei': 1}, 'james': 2},
-      'wendi': {'bec': {'theo': 3, 'max': 4}},
+      'wendi': {'bec': {'theo': 3.126, 'max': 4}},
       'liz': 'boo!',
       'donald': True,
       'price': Rate(1, 2, {'$': 1, 'item': -1})
@@ -17,7 +17,7 @@ records = [
     'date': date(2023, 1, 1),
     'cecil': {
       'robert': {'john': {'zenn': 7, 'rei': 8}, 'james': 9},
-      'wendi': {'bec': {'theo': 10, 'max': 11}},
+      'wendi': {'bec': {'theo': 10.124, 'max': 11}},
       'liz': 'who!',
       'donald': False,
       'price': Rate(2, 3, {'$': 1, 'item': -1})
@@ -27,14 +27,13 @@ records = [
     'date': date(2023, 1, 1),
     'cecil': {
       'robert': {'john': {'zenn': 14, 'rei': 15}, 'james': 16},
-      'wendi': {'bec': {'theo': 17, 'max': 18}},
+      'wendi': {'bec': {'theo': 17.124, 'max': 18}},
       'liz': 'phew!',
       'donald': True,
       'price': Rate(4, 5, {'$': 1, 'item': -1})
     }
   }
 ]
-
 
 table = Table()
 table.add_records(records)
