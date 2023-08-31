cc1a3407c49f98f05db3e0ded490dabcc3831c7c
John Forbes <john.robert.forbes@gmail.com>
2023-08-31 21:28:38 +1000

    Updated scrap.py.

--- scrap.py
+++ scrap.py
@@ -8,8 +8,8 @@ records = [
     'cecil': {
       'robert': {'john': {'zenn': 0, 'rei': 1}, 'james': 2},
       'wendi': {'bec': {'theo': 3, 'max': 4}},
-      'liz': 5,
-      'donald': 6,
+      'liz': 'boo!',
+      'donald': True,
       'price': Rate(1, 2, {'$': 1, 'item': -1})
     }
   },
@@ -18,8 +18,8 @@ records = [
     'cecil': {
       'robert': {'john': {'zenn': 7, 'rei': 8}, 'james': 9},
       'wendi': {'bec': {'theo': 10, 'max': 11}},
-      'liz': 12,
-      'donald': 13,
+      'liz': 'who!',
+      'donald': False,
       'price': Rate(2, 3, {'$': 1, 'item': -1})
     }
   },
@@ -28,8 +28,8 @@ records = [
     'cecil': {
       'robert': {'john': {'zenn': 14, 'rei': 15}, 'james': 16},
       'wendi': {'bec': {'theo': 17, 'max': 18}},
-      'liz': 19,
-      'donald': 20,
+      'liz': 'phew!',
+      'donald': True,
       'price': Rate(4, 5, {'$': 1, 'item': -1})
     }
   }
