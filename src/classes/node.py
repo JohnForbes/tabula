c8e8e7bab26a3b37719cec23eeb59f6350b82640
John Forbes <john.robert.forbes@gmail.com>
2023-08-31 22:04:22 +1000

    Added src/classes/node.py.

---
+++ src/classes/node.py
@@ -0,0 +1,53 @@
+from src.functions.strings.block.to_str import f as block_to_str
+from src.functions.node.block.make import f as make_block
+from src.functions.node.width.get import f as get_width
+from hak.pf import f as pf
+# from src.classes.table import Table
+
+class Node:
+  def __init__(self, name, table):
+    self.name = name
+    self.parent = None
+    self.children = set()
+    self.nodepath = tuple([name])
+    self.table = table
+
+  def add_child(self, child):
+    self.children.add(child)
+    child.parent = self
+    child.nodepath = tuple(list(self.nodepath) + list(child.nodepath))
+  
+  def add_children(self, children):
+    for child in children:
+      self.add_child(child)
+  
+  __str__ = lambda self: block_to_str(self.block)
+  block = property(make_block)
+  level = property(lambda s: 0 if not s.parent else s.parent.level+1)
+  width = property(get_width)
+
+f = lambda name, table: Node(name, table)
+
+# def t__init():
+#   x = {
+#     'name': 'node_name',
+#     'table': Table(),
+#   }
+#   return 0
+
+def t__str(): return 0
+def t_add_child(): return 0
+def t_add_children(): return 0
+def t_block(): return 0
+def t_level(): return 0
+def t_width(): return 0
+
+def t():
+  # if not t__init(): return pf('!t__init')
+  if not t__str(): return pf('!t__str')
+  if not t_add_child(): return pf('!t_add_child')
+  if not t_add_children(): return pf('!t_add_children')
+  if not t_block(): return pf('!t_block')
+  if not t_level(): return pf('!t_level')
+  if not t_width(): return pf('!t_width')
+  return 1
