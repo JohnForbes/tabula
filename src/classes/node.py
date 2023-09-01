3a6a92ce9980a87bc3f84c5e9728ecec013c36b5
John Forbes <john.robert.forbes@gmail.com>
2023-09-01 21:39:23 +1000

    Updated src/classes/node.py.

--- src/classes/node.py
+++ src/classes/node.py
@@ -1,8 +1,27 @@
-from src.functions.strings.block.to_str import f as block_to_str
-from src.functions.node.block.make import f as make_block
-from src.functions.node.width.get import f as get_width
 from hak.pf import f as pf
+from src.functions.ints.cell_value_widths.to_aggregate_width import f as aw
+from src.functions.nodes.sort_children_by_nodepath import f as sort_by_nodepath
+from src.functions.strings.block.hstack import f as hstack
+from src.functions.strings.block.vstack import f as vstack
 # from src.classes.table import Table
+
+def make_block(x):
+  top_block = [f' {x.name:^{x.width}} ']
+  return (
+    vstack([top_block, hstack([c.block for c in sort_by_nodepath(x)])])
+    if x.children else
+    top_block
+  )
+
+get_width = lambda node: max(
+  len(node.name),
+  aw([c.width for c in node.children]),
+  (
+    node.table.get_column(node.nodepath).width
+    if (node.nodepath, 0) in node.table.cells else
+    0
+  )
+)
 
 class Node:
   def __init__(self, name, table):
@@ -20,12 +39,17 @@ class Node:
 
 f = lambda name, table: Node(name, table)
 
-# def t__init():
-#   x = {
-#     'name': 'node_name',
-#     'table': Table(),
-#   }
-#   return 0
+def t__init():
+  from importlib import import_module
+  _Table = import_module('src.classes.table').Table
+  _table = _Table()
+  node = Node('node_name', _table)
+  if node.name != 'node_name': return pf("node.name != 'node_name'")
+  if node.children != set(): return pf("node.children != set()")
+  if node.nodepath != tuple(['node_name']):
+    return pf("node.nodepath != tuple(['node_name'])")
+  if node.table != _table: return pf("node.table != _table")
+  return 1
 
 def t__str(): return 0
 def t_add_child(): return 0
@@ -35,11 +59,11 @@ def t_level(): return 0
 def t_width(): return 0
 
 def t():
-  # if not t__init(): return pf('!t__init')
-  if not t__str(): return pf('!t__str')
-  if not t_add_child(): return pf('!t_add_child')
-  if not t_add_children(): return pf('!t_add_children')
-  if not t_block(): return pf('!t_block')
-  if not t_level(): return pf('!t_level')
-  if not t_width(): return pf('!t_width')
+  if not t__init(): return pf('!t__init')
+  # if not t__str(): return pf('!t__str')
+  # if not t_add_child(): return pf('!t_add_child')
+  # if not t_add_children(): return pf('!t_add_children')
+  # if not t_block(): return pf('!t_block')
+  # if not t_level(): return pf('!t_level')
+  # if not t_width(): return pf('!t_width')
   return 1
