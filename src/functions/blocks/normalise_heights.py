8d4ff1fba7d9f25874c02f5ed43c60cd72639e08
John Forbes <john.robert.forbes@gmail.com>
2023-09-01 21:41:23 +1000

    Updated src/functions/blocks/normalise_heights.py.

--- src/functions/blocks/normalise_heights.py
+++ src/functions/blocks/normalise_heights.py
@@ -1,8 +1,20 @@
 from src.functions.blocks.get_max_height import f as get_max_block_height
+from hak.pxyf import f as pxyf
 
-def f(blocks):
-  max_block_height = get_max_block_height(blocks)
-  for block in blocks:
+def f(x):
+  max_block_height = get_max_block_height(x)
+  for block in x:
     while len(block) < max_block_height:
       block.append(' '*len(block[0]))
-  return blocks
+  return x
+
+def t():
+  x = [
+    [     ' james '],
+    ['    john    ', '------------', ' rei | zenn ']
+  ]
+  y   = [
+    [     ' james ',      '       ',      '       '],
+    ['    john    ', '------------', ' rei | zenn ']
+  ]
+  return pxyf(x, y, f)
