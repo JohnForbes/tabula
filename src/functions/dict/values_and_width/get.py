c460ad345c93cc2c56c41c5ae305164f3accedc7
John Forbes <john.robert.forbes@gmail.com>
2023-09-02 19:06:52 +1000

    Updated src/functions/dict/values_and_width/get.py.

--- src/functions/dict/values_and_width/get.py
+++ src/functions/dict/values_and_width/get.py
@@ -1,16 +1,11 @@
 from hak.pxyf import f as pxyf
 
-from src.functions.dict.value_and_width.to_str import f as make_line_value
+from hak.one.dict.value_and_width.to_str import f as v_w_to_s
 
 # get_value_row_strings
-f = lambda x: [
-  make_line_value({'value': v, 'width': x['width']})
-  for v
-  in x['values']
-]
+f = lambda x: [v_w_to_s({'value': v, 'width': x['width']}) for v in x['values']]
 
-t = lambda: pxyf(
-  {'values': [0, 1, 2, 3], 'width': 4},
+t = lambda: pxyf({'values': [0, 1, 2, 3], 'width': 4},
   ['    0 ', '    1 ', '    2 ', '    3 '],
   f
 )
