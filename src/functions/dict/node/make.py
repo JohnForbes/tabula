08333069b891e4e973fe4cc8cc0e28422b0bb746
John Forbes <john.robert.forbes@gmail.com>
2023-09-03 09:12:08 +1000

    Removed src/functions/dict/node/make.py.

--- src/functions/dict/node/make.py
+++
@@ -1,9 +0,0 @@
-from hak.pxyf import f as pxyf
-
-f = lambda name: {'name': name, 'value': None, 'parent': None, 'children': []}
-
-t = lambda: pxyf(
-  'foo',
-  {'name': 'foo', 'value': None, 'parent': None, 'children': []},
-  f
-)
