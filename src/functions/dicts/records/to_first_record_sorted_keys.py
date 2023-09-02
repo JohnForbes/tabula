4e30ba21edabb6d25a378752e8dec0a4e2d96021
John Forbes <john.robert.forbes@gmail.com>
2023-09-02 19:17:06 +1000

    Removed src/functions/dicts/records/to_first_record_sorted_keys.py.

--- src/functions/dicts/records/to_first_record_sorted_keys.py
+++
@@ -1,7 +0,0 @@
-from hak.one.dict.get_sorted_keys import f as get_sorted_keys
-from hak.pxyf import f as pxyf
-
-# records_to_first_record_sorted_keys
-f = lambda x: get_sorted_keys(x[0])
-
-t = lambda: pxyf([{'c': 0, 'b': 1, 'a': 2}, {}], list('abc'), f)
