11689a4eacf16e89484d6b6fe46b44c5d6cd94d4
John Forbes <john.robert.forbes@gmail.com>
2023-09-02 17:58:09 +1000

    Updated src/functions/dict/value_and_width/to_str.py.

--- src/functions/dict/value_and_width/to_str.py
+++ src/functions/dict/value_and_width/to_str.py
@@ -1,4 +1,4 @@
-from hak.one.string.colour.bright.green import f as g
+from hak.one.string.colour.bright.green import f as green
 from hak.one.string.colour.decolour import f as decolour
 from hak.pf import f as pf
 from hak.pxyf import f as pxyf
@@ -10,7 +10,7 @@ def f(x):
   return f" {v:>{w}} "
 
 t_int = lambda: pxyf({'value': 12, 'width': 10}, '         12 ', f)
-t_yes = lambda: pxyf({'value': g('Y'), 'width': 4}, f"    {g('Y')} ", f)
+t_yes = lambda: pxyf({'value': green('Y'), 'width': 4}, f"    {green('Y')} ", f)
 
 def t():
   if not t_int(): return pf('!t_int')
